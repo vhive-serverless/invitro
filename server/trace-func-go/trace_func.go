7fbbac8607fa0afaecf8bde9eb75499194437e91
Hongyu <hongyuhe.cs@googlemail.com>
2021-12-27 14:13:18 +0100

    Rewrite memory allocation
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- server/trace-func-go/trace_func.go
+++ server/trace-func-go/trace_func.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net"
-
 	"time"
 
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
+	util "github.com/eth-easl/loader/internal"
 	rpc "github.com/eth-easl/loader/server"
-)
-
-var (
-	start time.Time
-	//* Use `make()` to allocate a static array (NOT a linked slice) on heap.
-	osPage     = [4 * 1024]byte{}
-	kb         = cap(osPage) / 4
-	serverPort = 80
 )
 
 type funcServer struct {
@@ -28,40 +20,33 @@ type funcServer struct {
 }
 
 func (s *funcServer) Execute(ctx context.Context, req *rpc.FaasRequest) (*rpc.FaasReply, error) {
-	start = time.Now()
-
+	start := time.Now()
 	runtimeRequested := req.RuntimeInMilliSec
-	memoryRequestedInKbs := req.MemoryInMebiBytes * uint32(kb) // MiB to KB.
 	timeoutSem := time.After(time.Duration(runtimeRequested) * time.Millisecond)
 
-	//* To avoid unecessary overhead, memory allocation is at the granularity of linux pages.
-	numPagesRequested := memoryRequestedInKbs/4 - 1
-	for i := 0; i < int(numPagesRequested); i++ {
-		select {
-		case <-timeoutSem:
-			return makeReply(uint32((i + 1) * 4 * kb)),
-				errors.New("timeout when allocating memory")
-		default:
-			//* Create a NEW page by copying the existing one.
-			newPage := osPage
-			//* Write to the first byte to consolidate virtual mem. into physical mem.
-			newPage[0] = byte(i)
-		}
+	//* To avoid unecessary overhead, memory allocation is at the granularity of os pages.
+	numPagesRequested := util.Mib2b(req.MemoryInMebiBytes) / uint32(unix.Getpagesize())
+	pages, err := unix.Mmap(-1, 0, int(numPagesRequested)*unix.Getpagesize(),
+		unix.PROT_WRITE, unix.MAP_ANON|unix.MAP_PRIVATE)
+	if err != nil {
+		log.Errorf("Failed to allocate requested memory: %v", err)
+		return &rpc.FaasReply{}, err
 	}
+	pages[0] = byte(1) //* Materialise allocated memory.
+	err = unix.Munmap(pages)
+	util.Check(err)
 
 	<-timeoutSem //* Fulfil requested runtime.
-	return makeReply(memoryRequestedInKbs), nil
-}
-
-func makeReply(memoryUsage uint32) *rpc.FaasReply {
 	return &rpc.FaasReply{
 		Message:           "", // Unused
 		LatencyInMicroSec: uint32(time.Since(start).Microseconds()),
-		MemoryUsageInKb:   memoryUsage,
-	}
+		MemoryUsageInKb:   util.B2Kib(numPagesRequested * uint32(unix.Getpagesize())),
+	}, nil
 }
 
 func main() {
+	serverPort := 80
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", serverPort))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
