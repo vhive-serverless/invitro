d03b84d06a404b866cac0b6054f2c8ef5b1a407d
Hongyu <hongyuhe.cs@googlemail.com>
2022-02-18 14:18:48 +0100

    Check system latencies instead of reponse times
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- pkg/metric/record.go
+++ pkg/metric/record.go
@@ -18,13 +18,13 @@ type MinuteInvocationRecord struct {
 type ExecutionRecord struct {
 	*sync.Mutex
 
-	Phase     int    `csv:"phase"`
-	Rps       int    `csv:"request_per_sec"`
-	Timestamp int64  `csv:"timestamp"`
-	FuncName  string `csv:"func_name"`
-	Latency   int64  `csv:"latency"`
-	Runtime   uint32 `csv:"runtime"`
-	Memory    uint32 `csv:"memory"`
-	Timeout   bool   `csv:"timeout"`
-	Failed    bool   `csv:"failed"`
+	Phase        int    `csv:"phase"`
+	Rps          int    `csv:"request_per_sec"`
+	Timestamp    int64  `csv:"timestamp"`
+	FuncName     string `csv:"func_name"`
+	ResponseTime int64  `csv:"response_time"` //* End-to-end latency.
+	Runtime      uint32 `csv:"runtime"`
+	Memory       uint32 `csv:"memory"`
+	Timeout      bool   `csv:"timeout"`
+	Failed       bool   `csv:"failed"`
 }
