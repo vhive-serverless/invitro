35ed0935d748926250d40fa627f214155a39ea47
Hongyu <hongyuhe.cs@googlemail.com>
2022-02-20 23:36:32 +0100

    Skip master when scraping metrics
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- scripts/setup/monitor_infra_metrics.sh
+++ scripts/setup/monitor_infra_metrics.sh
@@ -5,7 +5,7 @@ server_exec() {
 }
 
 echo 'Setting up monitoring components'
-# #* Deploy Metrics Server to k8s in namespace `kube-system`
+#* Deploy Metrics Server to k8s in namespace kube-system
 server_exec 'cd loader; kubectl apply -f config/metrics-server-components.yaml'
 
 #* Install helm
@@ -19,12 +19,12 @@ prefix="loader"
 server_exec "helm install -n monitoring $prefix prometheus-community/kube-prometheus-stack"
 sleep 5s
 
-# #* Set up port forwarding
+#* Set up port forwarding
 server_exec 'tmux new -s prometheusd -d'
-server_exec 'tmux send -t prometheusd "kubectl port-forward -n service/monitoring service/loader-kube-prometheus-st-prometheus 9090" ENTER'
+server_exec 'tmux send -t prometheusd "kubectl port-forward -n monitoring service/loader-kube-prometheus-sta-prometheus 9090" ENTER'
 
 #* Find all service monitors
-server_exec "sudo kubectl -n monitoring patch prometheus loader-kube-prometh-prometheus --type json -p '[{"op": "replace", "path": "/spec/serviceMonitorSelector", "value": {}}, {"op": "replace", "path": "/spec/serviceMonitorNamspaceSelector", "value": {}}]'"
+server_exec "sudo kubectl -n monitoring patch prometheus loader-kube-prometheus-sta-prometheus --type json -p '[{"op": "replace", "path": "/spec/serviceMonitorSelector", "value": {}}, {"op": "replace", "path": "/spec/serviceMonitorNamspaceSelector", "value": {}}]'"
 #* Set the node exporter speed to 500ms
 server_exec "sudo kubectl -n monitoring patch ServiceMonitor loader-prometheus-node-exporter --type json -p '[{"op": "add", "path": "/spec/endpoints/0/interval", "value": "0.5s"}]'"
 #* Set up service monitors for knative
