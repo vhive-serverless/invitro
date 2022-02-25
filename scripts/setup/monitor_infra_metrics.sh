8e02cdf46bda6d749410210db22d201d8b4ad6de
Hongyu <hongyuhe.cs@googlemail.com>
2022-02-25 08:10:59 +0100

    Add option for RPS starting point
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- scripts/setup/monitor_infra_metrics.sh
+++ scripts/setup/monitor_infra_metrics.sh
@@ -5,7 +5,7 @@ server_exec() {
 }
 
 echo 'Setting up monitoring components'
-#* Deploy Metrics Server to k8s in namespace kube-system
+#* Deploy Metrics Server to k8s in namespace kube-system (`kubectl get all -n kube-system -owide`)
 server_exec 'cd loader; kubectl apply -f config/metrics-server-components.yaml'
 
 #* Install helm
