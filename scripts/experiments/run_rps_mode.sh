c212914c4df861ba8897d80ec53710a430eab638
Hongyu <hongyuhe.cs@googlemail.com>
2022-03-26 01:03:11 +0100

    Update run scripts
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- scripts/experiments/run_rps_mode.sh
+++ scripts/experiments/run_rps_mode.sh
@@ -1,1 +1,2 @@
-make ARGS='-mode stress -start 1 -step 1 -slot 60 -server busy' run 2>&1 | tee stress.log
+#!/usr/bin/env bash
+make ARGS='-mode stress -start 1 -step 1 -slot 120 -server trace' run 2>&1 | tee stress.log
