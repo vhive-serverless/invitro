5e8795dacf23633f92af11c68e65c8937fb84007
Hongyu <hongyuhe.cs@googlemail.com>
2022-04-07 16:34:56 +0200

    Relax warmup requirements
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- scripts/warmup/reset_kn_global.sh
+++ scripts/warmup/reset_kn_global.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 kubectl patch configmap config-autoscaler -n knative-serving \
-    -p '{"data":{"initial-scale":"1","enable-scale-to-zero":"true","max-scale-up-rate": "1000.0"}}'
+    -p '{"data":{"initial-scale":"1","enable-scale-to-zero":"true","max-scale-up-rate": "1000.0","max-scale-down-rate": "2.0"}}'
