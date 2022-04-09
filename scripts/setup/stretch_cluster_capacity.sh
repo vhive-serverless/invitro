aa59e2d98a94c80ada9b04b57ead2d01943e47b2
Hongyu <hongyuhe.cs@googlemail.com>
2022-04-09 16:31:17 +0200

    Restrict pod limit to 200 sharp
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- scripts/setup/stretch_cluster_capacity.sh
+++ scripts/setup/stretch_cluster_capacity.sh
@@ -1,2 +1,2 @@
-echo "maxPods: 500" > >(sudo tee -a /var/lib/kubelet/config.yaml >/dev/null)
+echo "maxPods: 200" > >(sudo tee -a /var/lib/kubelet/config.yaml >/dev/null)
 sudo systemctl restart kubelet
