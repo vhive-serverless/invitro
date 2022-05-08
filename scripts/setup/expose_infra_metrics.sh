19352b81e01f878b7f60e2c4cb7a9efa06f5878a
Hongyu <hongyuhe.cs@googlemail.com>
2022-05-08 15:09:41 +0200

    Separate script for creating large nodes
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- scripts/setup/expose_infra_metrics.sh
+++ scripts/setup/expose_infra_metrics.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 MASTER_NODE=$1
+USE_LARGE=$2
+
 server_exec() { 
 	ssh -oStrictHostKeyChecking=no -p 22 $MASTER_NODE $1;
 }
@@ -23,8 +25,14 @@ server_exec() {
 	#* Apply the ServiceMonitors/PodMonitors to collect metrics from Knative.
 	#* The ports of the control manager and scheduler are mapped in a way that prometheus default installation can find them. 
 	server_exec 'cd loader; kubectl apply -f config/prometh_kn.yaml'
+
 	#* Bind addresses of the control manager and scheduler to "0.0.0.0" so that prometheus can scrape them from any domains.
-	server_exec 'cd loader; sudo kubeadm upgrade apply --config config/kubeadm_init.yaml --ignore-preflight-errors all --force --v=5'
+	if [[ ! -z $USE_LARGE && $USE_LARGE == 'large' ]]; 
+	then
+		server_exec 'cd loader; sudo kubeadm upgrade apply --config config/kubeadm_init_large.yaml --ignore-preflight-errors all --force --v=5'	
+	else
+		server_exec 'cd loader; sudo kubeadm upgrade apply --config config/kubeadm_init.yaml --ignore-preflight-errors all --force --v=5'
+	fi
 
 
 	#* Change scrape intervals to 2s for all used monitors.
