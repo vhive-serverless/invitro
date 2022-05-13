84f041e5527abd21e6d191f8eee2e19e90c4bcd7
Hongyu <hongyuhe.cs@googlemail.com>
2022-05-14 00:25:31 +0200

    Take into account instruction pipeline
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- server/benchmark/drive_benchmark.py
+++ server/benchmark/drive_benchmark.py
@@ -3,23 +3,27 @@ import os
 if __name__ == "__main__":
     print('Benchmarking function timing ...\n')
 
-    cmd = "go test -bench=BenchmarkIterations -benchtime 1ms -cpu=1 -count=20 | tail -n +5 | head -n -2 | awk '{print $2}'"
+    cmd = "go test -bench=BenchmarkColdIterations -benchtime 1ms -cpu=1 -count=10 | tail -n +5 | head -n -2 | awk '{print $2}'"
     results = list(map(int, os.popen(cmd).read().strip().split()))
-    avg_iter_per_1ms = int(sum(results) / len(results))
+    cold_iter_per_1ms = int(sum(results) / len(results))
+    print(f"{cold_iter_per_1ms=}")
 
-    print(f"{avg_iter_per_1ms=}")
+    cmd = "go test -bench=BenchmarkWarmIterations -benchtime 1ms -cpu=1 -count=20 | tail -n +5 | head -n -2 | awk '{print $2}'"
+    results = list(map(int, os.popen(cmd).read().strip().split()))
+    warm_iter_per_1ms = int(sum(results) / len(results))
+    print(f"{warm_iter_per_1ms=}\n")
 
     cmd = "go test -bench=BenchmarkTiming -benchtime 1000ms -cpu=1 -count=20 | tail -n +5 | head -n -2 | awk '{print $2}'"
     results = list(map(int, os.popen(cmd).read().strip().split()))
     avg_duration = int(sum(results) / len(results))
-    print(f"Expected #iterations: 1000. Actaul #iterations: {avg_duration} (avg)")
+    print(f"Expected #iterations: 1000.\t Actaul #iterations: {avg_duration} (avg)")
     
     cmd = "go test -bench=BenchmarkTiming -benchtime 100ms -cpu=1 -count=50 | tail -n +5 | head -n -2 | awk '{print $2}'"
     results = list(map(int, os.popen(cmd).read().strip().split()))
     avg_duration = int(sum(results) / len(results))
-    print(f"Expected #iterations: 100. Actaul #iterations: {avg_duration} (avg)")
+    print(f"Expected #iterations: 100.\t Actaul #iterations: {avg_duration} (avg)")
 
     cmd = "go test -bench=BenchmarkTiming -benchtime 10ms -cpu=1 -count=100 | tail -n +5 | head -n -2 | awk '{print $2}'"
     results = list(map(int, os.popen(cmd).read().strip().split()))
     avg_duration = int(sum(results) / len(results))
-    print(f"Expected #iterations: 10. Actaul #iterations: {avg_duration} (avg)\n")
+    print(f"Expected #iterations: 10.\t Actaul #iterations: {avg_duration} (avg)\n")
