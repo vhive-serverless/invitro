594c85dd3e4048cdd361f91dc3da80e0ada00657
Hongyu <hongyuhe.cs@googlemail.com>
2022-05-22 15:45:02 +0200

    Increase RPS warmup period
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

--- scripts/experiments/drive_trace_mode.py
+++ scripts/experiments/drive_trace_mode.py
@@ -5,6 +5,7 @@ import ntpath
 import sys
 import os
 import os.path
+from time import sleep
 
 def main(argv):
     _, duration, cluster, server = argv
@@ -16,7 +17,7 @@ def main(argv):
 
     tracef = list(map(ntpath.basename, sorted(glob('data/traces/*.csv'))))
     sizes = []
-    for f in tracef[::3]:
+    for f in tracef[::3]: #* Duplicated sizes (3 times).
         sizes.append(int(f.split('_')[0]))
     sizes.sort()
 
@@ -26,19 +27,18 @@ def main(argv):
     for size in sizes:
         # for _ in range(repeat):
         os.system('make -i clean')
+        sleep(5)
+        try:
+            cmd = f"make ARGS='-sample {size} -duration {duration} -cluster {cluster} -server {server} -warmup' run 2>&1 | tee cap_{size}.log"
+            print(cmd)
+            os.system(command=cmd)
+            # if glob(flagf): break
+        except KeyboardInterrupt:
+            print('Experiment interrupted')
+            try: sys.exit(0)
+            except SystemExit: os._exit(0)
         
-        command = f"make ARGS='-sample {size} -duration {duration} -cluster {cluster} -server {server} -warmup' run 2>&1 | tee cap_{size}.log"
-        print(command)
-        os.system(command=command)
-        # if glob(flagf):
-        #     break
+        
 
 if __name__ == '__main__':
-    try:
-        main(sys.argv)
-    except KeyboardInterrupt:
-        print('Experiment interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+    main(sys.argv)
