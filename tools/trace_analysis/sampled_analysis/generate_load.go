9f719884ff37955abc0fd497eb707542eb1c1475
Dohyun Park <dohyun1357@gmail.com>
2022-08-09 13:28:18 +0200

    fix notebook and cleanup code
    Signed-off-by: Dohyun Park <dohyun1357@gmail.com>

--- tools/trace_analysis/sampled_analysis/generate_load.go
+++ tools/trace_analysis/sampled_analysis/generate_load.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"math"
 	"os"
-	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -15,38 +14,26 @@ import (
 	tc "github.com/eth-easl/loader/pkg/trace"
 )
 
-func convertMemoryToCPU(memory int) float64 {
-	// Numbers taken from: https://cloud.google.com/functions/pricing
-	if memory <= 128 {
-		return 0.083
-	} else if memory <= 256 {
-		return 0.167
-	} else if memory <= 512 {
-		return 0.333
-	} else if memory <= 1024 {
-		return 0.583
-	} else if memory <= 2048 {
-		return 1
-	} else {
-		return 2
-	}
-}
+var (
+	invPath    = flag.String("invPath", "../../../data/traces/traces/1500_inv.csv", "path to invocation trace")
+	runPath    = flag.String("runPath", "../../../data/traces/traces/1500_run.csv", "path to runtime trace")
+	memPath    = flag.String("memPath", "../../../data/traces/traces/1500_mem.csv", "path to memory trace")
+	outputFile = flag.String("outputFile", "output.csv", "name of output file")
+)
 
 func main() {
+	flag.Parse()
 	var wg sync.WaitGroup
 	var wg2 sync.WaitGroup
+
 	gen.InitSeed(42)
 	writer := make(chan string)
-	sampleSize := 1500
+
 	var traces tc.FunctionTraces
 
-	invPath := "data/traces/" + strconv.Itoa(sampleSize) + "_inv.csv"
-	runPath := "data/traces/" + strconv.Itoa(sampleSize) + "_run.csv"
-	memPath := "data/traces/" + strconv.Itoa(sampleSize) + "_mem.csv"
-
-	traces = tc.ParseInvocationTrace(invPath, 1440)
-	tc.ParseDurationTrace(&traces, runPath)
-	tc.ParseMemoryTrace(&traces, memPath)
+	traces = tc.ParseInvocationTrace(*invPath, 1440)
+	tc.ParseDurationTrace(&traces, *runPath)
+	tc.ParseMemoryTrace(&traces, *memPath)
 
 	log.Info("Traces contain the following: ", len(traces.Functions), " functions")
 
@@ -55,10 +42,9 @@ func main() {
 
 	totalDurationMinutes := len(traces.TotalInvocationsPerMinute)
 
-	minute := 0
 	wg2.Add(1)
 	go func() {
-		f, err := os.OpenFile("output.csv", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+		f, err := os.OpenFile(*outputFile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 		if err != nil {
 			wg2.Done()
 			panic(err)
@@ -66,22 +52,16 @@ func main() {
 		f.WriteString("millisecond,functionHash,runtime,memory,maxMemory,cpu,maxCpu\n")
 
 		for msg := range writer {
-			if msg == "done" {
-				break
-			}
 			f.WriteString(msg)
 		}
-		fmt.Println("Writer done")
 		wg2.Done()
 		f.Close()
 	}()
 
-	for ; minute < int(totalDurationMinutes); minute++ {
-		log.Infof("Processing minute %d", minute)
+	for minute := 0; minute < int(totalDurationMinutes); minute++ {
 		var iats []float64
 		var numFuncInvokedThisMinute int64 = 0
 
-		rps := int(math.Ceil(float64(traces.TotalInvocationsPerMinute[minute]) / 60.0))
 		numInvocatonsThisMinute := traces.TotalInvocationsPerMinute[minute]
 		iats = gen.GenerateOneMinuteInterarrivalTimesInMicro(
 			numInvocatonsThisMinute,
@@ -89,32 +69,29 @@ func main() {
 		)
 		if numInvocatonsThisMinute < 1 {
 			continue
-		} else {
-			wg.Add(1)
-			go func(minute int, iats []float64) {
-				log.Infof("Minute[%d]\t RPS=%d", minute, rps)
-				for nxt := 0; nxt < numInvocatonsThisMinute; nxt++ {
-					var runtimeRequested, memoryRequested int
-					sum := 0.0
-					for i := 0; i <= nxt; i++ {
-						sum += iats[i]
-					}
-
-					tt := time.Duration(minute)*time.Minute + time.Duration(sum)*time.Microsecond
-
-					atomic.AddInt64(&numFuncInvokedThisMinute, 1)
-					funcIndx := invocationsEachMinute[minute][nxt]
-					function := traces.Functions[funcIndx]
-					runtimeRequested, memoryRequested = gen.GenerateExecutionSpecs(function)
-					writer <- fmt.Sprintf("%v,%v,%v,%v,%v,%v,%v\n", int(tt/time.Millisecond), function.HashApp, runtimeRequested, memoryRequested, function.MemoryStats.Percentile100, convertMemoryToCPU(memoryRequested), convertMemoryToCPU(function.MemoryStats.Percentile100))
-				}
-				log.Info("Minute[", minute, "]\t", "exited")
-				wg.Done()
-			}(minute, iats)
 		}
+		wg.Add(1)
+		go func(minute int, iats []float64) {
+			for nxt := 0; nxt < numInvocatonsThisMinute; nxt++ {
+				var runtimeRequested, memoryRequested int
+				var sum float64
+
+				for i := 0; i <= nxt; i++ {
+					sum += iats[i]
+				}
+
+				tt := time.Duration(minute)*time.Minute + time.Duration(sum)*time.Microsecond
+
+				atomic.AddInt64(&numFuncInvokedThisMinute, 1)
+				funcIndx := invocationsEachMinute[minute][nxt]
+				function := traces.Functions[funcIndx]
+				runtimeRequested, memoryRequested = gen.GenerateExecutionSpecs(function)
+				writer <- fmt.Sprintf("%v,%v,%v,%v,%v,%v,%v\n", int(tt/time.Millisecond), function.HashApp, runtimeRequested, memoryRequested, function.MemoryStats.Percentile100, tc.ConvertMemoryToCpu(memoryRequested), tc.ConvertMemoryToCpu(function.MemoryStats.Percentile100))
+			}
+			wg.Done()
+		}(minute, iats)
 	}
-	log.Info("Waiting for all goroutines to finish")
 	wg.Wait()
-	writer <- "done"
+	close(writer)
 	wg2.Wait()
 }
--- tools/trace_analysis/sampled_analysis/make_timeline.go
+++ tools/trace_analysis/sampled_analysis/make_timeline.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -10,22 +11,48 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	f, err := os.Open("output.csv")
-	// maxTime := 24 * 60 * 60 * 1000
-	maxTime := 86454632 + 1
-	funcCnt := make([]int, maxTime)
-	memUsg := make([]int, maxTime)
-	memReq := make([]int, maxTime)
-	cpuUsg := make([]float64, maxTime)
-	cpuReq := make([]float64, maxTime)
+var (
+	inputFile  = flag.String("inputFile", "output.csv", "name of input file")
+	outputFile = flag.String("outputFile", "processed.csv", "name of output file")
+)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+func main() {
+	flag.Parse()
+	f, err := os.Open(*inputFile)
 	defer f.Close()
 
+	var maxTime int
+
 	csvReader := csv.NewReader(f)
+	if _, err := csvReader.Read(); err != nil {
+		log.Fatal(err)
+	}
+	for {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		start, _ := strconv.Atoi(rec[0])
+		runtime, _ := strconv.Atoi(rec[2])
+		if start+runtime > maxTime {
+			maxTime = start + runtime
+		}
+	}
+	log.Infof("Max time: %d", maxTime)
+
+	funcCnt := make([]int, maxTime+1)
+	memUsg := make([]int, maxTime+1)
+	memReq := make([]int, maxTime+1)
+	cpuUsg := make([]float64, maxTime+1)
+	cpuReq := make([]float64, maxTime+1)
+
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		log.Fatal(err)
+	}
+
 	if header, err := csvReader.Read(); err != nil {
 		log.Fatal(err)
 	} else {
@@ -59,13 +86,13 @@ func main() {
 	}
 	log.Info("Done reading, writing to file")
 
-	ff, err := os.OpenFile("processed.csv", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+	ff, err := os.OpenFile(*outputFile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
 
 	ff.WriteString("timestamp,funcCnt,memUsg,memReq,cpuUsg,cpuReq\n")
-	for i := 0; i < maxTime; i++ {
+	for i := 0; i <= maxTime; i++ {
 		ff.WriteString(fmt.Sprintf("%d,%d,%d,%d,%f,%f\n", i, funcCnt[i], memUsg[i], memReq[i], cpuUsg[i], cpuReq[i]))
 	}
 	log.Infof("Done writing to file")
