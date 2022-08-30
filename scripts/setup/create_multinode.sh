cb50960707ae583a7dc7bc80ec5e583cbfbfb580
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2022-08-30 16:50:52 +0200

    Unified container/firecracker setup script
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

---
+++ scripts/setup/create_multinode.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+MASTER_NODE=$1
+
+DIR="$(pwd)/scripts/setup/"
+
+source "$DIR/setup.cfg"
+
+# OPERATION_MODE
+if [ "$OPERATION_MODE" = "container" ]
+then
+    OPERATION_MODE="stock-only"
+elif [ $OPERATION_MODE = "container_large" ]
+then
+    OPERATION_MODE="stock-only"
+elif [ $OPERATION_MODE = "firecracker" ]
+then
+    OPERATION_MODE=""
+else
+    echo "Unsupported operation mode"
+    exit 1
+fi
+
+server_exec() {
+	ssh -oStrictHostKeyChecking=no -p 22 "$1" "$2";
+}
+
+#* Run initialisation on a node.
+common_init() {
+	server_exec $1 "git clone --branch=$VHIVE_BRANCH https://github.com/ease-lab/vhive"
+	server_exec $1 "cd; ./vhive/scripts/cloudlab/setup_node.sh $OPERATION_MODE"
+	server_exec $1 'tmux new -s containerd -d'
+	server_exec $1 'tmux send -t containerd "sudo containerd 2>&1 | tee ~/containerd_log.txt" ENTER'
+	server_exec $1 'sudo ntpdate -s ops.emulab.net' # synchronize clock across nodes
+}
+
+{
+	#* Set up all nodes including the master.
+	for node in "$@"
+	do
+		echo $node
+		common_init "$node" &
+	done
+	wait $!
+
+	LOGIN_TOKEN=""
+	IS_MASTER=1
+	for node in "$@"
+	do
+		server_exec() {
+			ssh -oStrictHostKeyChecking=no -p 22 "$node" "$1";
+		}
+		if [ $IS_MASTER -eq 1 ]
+		then
+			echo "Setting up master node: $node"
+			IS_MASTER=0
+			server_exec 'wget -q https://dl.google.com/go/go1.17.linux-amd64.tar.gz >/dev/null'
+			server_exec 'sudo rm -rf /usr/local/go && sudo tar -C /usr/local/ -xzf go1.17.linux-amd64.tar.gz >/dev/null'
+			# server_exec 'sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev'
+			server_exec 'echo "export PATH=$PATH:/usr/local/go/bin" >> .profile'
+
+			server_exec 'tmux new -s runner -d'
+			server_exec 'tmux new -s kwatch -d'
+			server_exec 'tmux new -s master -d'
+			MN_CLUSTER="./vhive/scripts/cluster/create_multinode_cluster.sh ${OPERATION_MODE}"
+			server_exec "tmux send -t master \"$MN_CLUSTER\" ENTER"
+
+			# Get the join token from k8s.
+			while [ ! "$LOGIN_TOKEN" ]
+			do
+				sleep 1s
+				server_exec 'tmux capture-pane -t master -b token'
+				LOGIN_TOKEN="$(server_exec 'tmux show-buffer -b token | grep -B 3 "All nodes need to be joined"')"
+			done
+			# cut of last line
+			LOGIN_TOKEN=${LOGIN_TOKEN%[$'\t\r\n']*}
+			# remove the \
+			LOGIN_TOKEN=${LOGIN_TOKEN/\\/}
+			# remove all remaining tabs, line ends and returns
+			LOGIN_TOKEN=${LOGIN_TOKEN//[$'\t\r\n']}
+
+		else
+			echo "Setting up worker node: $node"
+			server_exec "./vhive/scripts/cluster/setup_worker_kubelet.sh $OPERATION_MODE"
+
+			#* We don't need vhive in container mode.
+			if [ "$OPERATION_MODE" = "" ] # empty string corresponds to firecracker
+			then
+        server_exec 'cd vhive; source /etc/profile && go build'
+        server_exec 'tmux new -s firecracker -d'
+        server_exec 'tmux send -t firecracker "sudo PATH=$PATH /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml 2>&1 | tee ~/firecracker_log.txt" ENTER'
+        server_exec 'tmux new -s vhive -d'
+        server_exec 'tmux send -t vhive "cd vhive" ENTER'
+        server_exec 'tmux send -t vhive "sudo ./vhive 2>&1 | tee ~/vhive_log.txt" ENTER'
+			fi
+		fi
+	done
+
+	IS_MASTER=1
+	for node in "$@"
+	do
+		if [ $IS_MASTER -eq 1 ]
+		then
+			IS_MASTER=0
+		else
+			ssh -oStrictHostKeyChecking=no -p 22 $node "sudo ${LOGIN_TOKEN}"
+			echo "Worker node $node joined the cluster."
+
+			#* Disable worker turbo boost for better timing.
+			server_exec './vhive/scripts/turbo_boost.sh disable'
+			#* Disable worker hyperthreading (security).
+			server_exec 'echo off | sudo tee /sys/devices/system/cpu/smt/control'
+
+			#* Stretch the capacity of the worker node to 240 (k8s default: 110).
+			#* Empirically, this gives us a max. #pods being 240-40=200.
+			echo "Streching node capacity for $node."
+			server_exec 'echo "maxPods: 240" > >(sudo tee -a /var/lib/kubelet/config.yaml >/dev/null)'
+			server_exec 'sudo systemctl restart kubelet'
+
+			#* Rejoin has to be performed although errors will be thrown. Otherwise, restarting the kubelet will cause the node unreachable for some reason.
+			ssh -oStrictHostKeyChecking=no -p 22 $node "sudo ${LOGIN_TOKEN} > /dev/null 2>&1"
+			echo "Worker node $node joined the cluster (again :P)."
+		fi
+	done
+
+	server_exec() {
+		ssh -oStrictHostKeyChecking=no -p 22 $MASTER_NODE $1;
+	}
+
+	#* Notify the master that all nodes have been registered
+	server_exec 'tmux send -t master "y" ENTER'
+	echo "Master node $MASTER_NODE finalised."
+
+	#* Setup github authentication.
+	ACCESS_TOKEH="$(cat $GITHUB_TOKEN)"
+
+	server_exec 'echo -en "\n\n" | ssh-keygen -t rsa'
+	server_exec 'ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts'
+
+	server_exec 'curl -H "Authorization: token '"$ACCESS_TOKEH"'" --data "{\"title\":\"'"key:\$(hostname)"'\",\"key\":\"'"\$(cat ~/.ssh/id_rsa.pub)"'\"}" https://api.github.com/user/keys'
+
+	#* Get loader and dependencies.
+	server_exec "git clone --branch=$LOADER_BRANCH git@github.com:eth-easl/loader.git"
+	server_exec 'echo -en "\n\n" | sudo apt-get install python3-pip python-dev'
+	server_exec 'cd; cd loader; pip install -r config/requirements.txt'
+
+	source $DIR/taint.sh
+
+	# force placement of metrics collectors and instrumentation on the master node
+	taint_workers $MASTER_NODE
+	$DIR/expose_infra_metrics.sh $MASTER_NODE
+	untaint_workers $MASTER_NODE
+
+	#* Disable master turbo boost.
+	server_exec 'bash loader/scripts/setup/turbo_boost.sh disable'
+	#* Disable master hyperthreading.
+	server_exec 'echo off | sudo tee /sys/devices/system/cpu/smt/control'
+	#* Create CGroup.
+	server_exec 'sudo bash loader/scripts/isolation/define_cgroup.sh'
+
+	taint_master $MASTER_NODE
+
+	echo "Logging in master node $MASTER_NODE"
+	ssh -p 22 $MASTER_NODE
+	exit
+}
