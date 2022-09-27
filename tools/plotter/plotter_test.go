15048dc76bb5cc4b3d12e4ee2f03e1e0d54d30eb
Dmitrii Ustiugov <dmitrii.ustiugov@ed.ac.uk>
2022-09-27 14:52:15 +0200

    Refined failed invocations filtering
    Other minor changes in plotting, minor refactoring
    
    Signed-off-by: Dmitrii Ustiugov <dmitrii.ustiugov@ed.ac.uk>

--- tools/plotter/plotter_test.go
+++ tools/plotter/plotter_test.go
@@ -9,10 +9,11 @@ import (
 func TestPlotter(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
 
-	records := parseFiles("./test-data")
+	records, failedNum := parseFiles("./test-data")
 
 	log.Debugf("Obtained %d records.", len(records))
 	require.Equal(t, len(records), 2)
+	require.Equal(t, failedNum, 3)
 
 	plotFig("./test-out", records)
 }
