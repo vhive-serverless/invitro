bfb94699fd9adca13e62fb5ee0ec690472a4560f
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2022-09-27 15:43:51 +0200

    Exponential IAT distribution with tests
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/generate/specification_test.go
+++ pkg/generate/specification_test.go
@@ -26,87 +26,107 @@ func TestSerialGenerateIAT(t *testing.T) {
 		duration        int // s
 		invocations     []int
 		iatDistribution IatDistribution
-		expectedPoints  []float64 // μs
+		expectedPoints  [][]float64 // μs
 	}{
+		{
+			testName:        "no_invocations",
+			invocations:     []int{5},
+			iatDistribution: Equidistant,
+			expectedPoints:  [][]float64{},
+		},
 		{
 			testName:        "1min_5ipm_equidistant",
 			invocations:     []int{5},
 			iatDistribution: Equidistant,
-			expectedPoints: []float64{
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				12000000,
+			expectedPoints: [][]float64{
+				{
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+				},
 			},
 		},
 		{
 			testName:        "5min_5ipm_equidistant",
 			invocations:     []int{5, 5, 5, 5, 5},
 			iatDistribution: Equidistant,
-			expectedPoints: []float64{
-				// min 1
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				// min 2
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				// min 3
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				// min 4
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				// min 5
-				12000000,
-				12000000,
-				12000000,
-				12000000,
-				12000000,
+			expectedPoints: [][]float64{
+				{
+					// min 1
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+				},
+				{
+					// min 2
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+				},
+				{
+					// min 3
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+				},
+				{
+					// min 4
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+				},
+				{
+					// min 5
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+					12000000,
+				},
 			},
 		},
 		{
 			testName:        "1min_25ipm_uniform",
 			invocations:     []int{25},
 			iatDistribution: Uniform,
-			expectedPoints: []float64{
-				3062124.611863,
-				3223056.707367,
-				3042558.740794,
-				2099765.805752,
-				375008.683565,
-				3979289.345154,
-				1636869.797787,
-				1169442.102841,
-				2380243.616007,
-				2453428.612640,
-				1704231.066313,
-				42074.939233,
-				3115643.026141,
-				3460047.444726,
-				2849475.331077,
-				3187546.011741,
-				2950391.492700,
-				622524.819620,
-				2161625.000293,
-				2467158.610498,
-				3161216.965226,
-				120925.338482,
-				3461650.068734,
-				3681772.563419,
-				3591929.298027,
+			expectedPoints: [][]float64{
+				{
+					3062124.611863,
+					3223056.707367,
+					3042558.740794,
+					2099765.805752,
+					375008.683565,
+					3979289.345154,
+					1636869.797787,
+					1169442.102841,
+					2380243.616007,
+					2453428.612640,
+					1704231.066313,
+					42074.939233,
+					3115643.026141,
+					3460047.444726,
+					2849475.331077,
+					3187546.011741,
+					2950391.492700,
+					622524.819620,
+					2161625.000293,
+					2467158.610498,
+					3161216.965226,
+					120925.338482,
+					3461650.068734,
+					3681772.563419,
+					3591929.298027,
+				},
 			},
 		},
 		{
@@ -114,6 +134,40 @@ func TestSerialGenerateIAT(t *testing.T) {
 			invocations:     []int{1000000},
 			iatDistribution: Uniform,
 			expectedPoints:  nil,
+		},
+		{
+			testName:        "1min_25ipm_exponential",
+			invocations:     []int{25},
+			iatDistribution: Exponential,
+			expectedPoints: [][]float64{
+				{
+					1311929.341329,
+					3685871.430916,
+					1626476.996595,
+					556382.014270,
+					30703.105102,
+					3988584.779392,
+					2092271.836277,
+					1489855.293253,
+					3025094.199801,
+					2366337.4678820,
+					40667.5994150,
+					2778945.4898700,
+					4201722.5747150,
+					5339421.1460450,
+					3362048.1584080,
+					939526.5236740,
+					1113771.3822940,
+					4439636.5676460,
+					4623026.1098310,
+					2082985.6557600,
+					45937.1189860,
+					4542253.8756200,
+					2264414.9939920,
+					3872560.8680640,
+					179575.4708620,
+				},
+			},
 		},
 		{
 			testName:        "1min_1000000ipm_exponential",
@@ -130,55 +184,56 @@ func TestSerialGenerateIAT(t *testing.T) {
 		t.Run(test.testName, func(t *testing.T) {
 			InitSeed(seed)
 
-			result := GenerateIAT(test.invocations, test.iatDistribution)
+			IAT, nonScaledDuration := GenerateIAT(test.invocations, test.iatDistribution)
 			failed := false
 
-			if testForSpillover(len(test.invocations), result) {
+			if hasSpillover(IAT) {
 				t.Error("Generated IAT does not fit in the " + strconv.Itoa(len(test.invocations)) + " minutes time window.")
 			}
 
 			if test.expectedPoints != nil {
-				for i := 0; i < len(result); i++ {
-					if math.Abs(result[i]-test.expectedPoints[i]) > epsilon {
-						fmt.Printf("got: %f, expected: %f\n", result[i], test.expectedPoints[i])
+				for min := 0; min < len(test.expectedPoints); min++ {
+					for i := 0; i < len(IAT[min]); i++ {
+						if math.Abs(IAT[min][i]-test.expectedPoints[min][i]) > epsilon {
+							fmt.Printf("got: %f, expected: %f\n", IAT[min][i], test.expectedPoints[min][i])
 
-						failed = true
-						// no break statement for debugging purpose
+							failed = true
+							// no break statement for debugging purpose
+						}
 					}
 				}
 
 				if failed {
 					t.Error("Test " + test.testName + " has failed due to incorrectly generated IAT.")
 				}
-			} else {
-				satisfiesDistribution := checkDistribution(result, test.iatDistribution)
+			}
 
-				if !satisfiesDistribution {
-					t.Error("The provided sample does not satisfy the given distribution.")
-				}
+			if test.iatDistribution != Equidistant && !checkDistribution(IAT, nonScaledDuration, test.iatDistribution) {
+				t.Error("The provided sample does not satisfy the given distribution.")
 			}
 		})
 	}
 }
 
-func testForSpillover(maxTime int, data []float64) bool {
-	sum := 0.0
-	epsilon := 1e-3
+func hasSpillover(data [][]float64) bool {
+	for min := 0; min < len(data); min++ {
+		sum := 0.0
+		epsilon := 1e-3
 
-	for i := 0; i < len(data); i++ {
-		sum += data[i]
-	}
+		for i := 0; i < len(data[min]); i++ {
+			sum += data[min][i]
+		}
 
-	if math.Abs(sum-float64(maxTime)*60*oneSecondInMicro) > epsilon {
 		fmt.Printf("Total execution time: %f μs\n", sum)
-
-		return true
-	} else {
-		return false
+		if math.Abs(sum-60*oneSecondInMicro) > epsilon {
+			return true
+		}
 	}
+
+	return false
 }
 
-func checkDistribution(data []float64, distribution IatDistribution) bool {
+func checkDistribution(data [][]float64, nonScaledDuration []float64, distribution IatDistribution) bool {
 	// PREPARING ARGUMENTS
 	var dist string
 	inputFile := "test_data.txt"
@@ -192,39 +247,43 @@ func checkDistribution(data []float64, distribution IatDistribution) bool {
 		panic("Unsupported distribution check")
 	}
 
-	// WRITING DISTRIBUTION TO TEST
-	f, err := os.Create(inputFile)
-	if err != nil {
-		panic("err")
-	}
+	result := false
 
-	defer f.Close()
+	for min := 0; min < len(data); min++ {
+		// WRITING DISTRIBUTION TO TEST
+		f, err := os.Create(inputFile)
+		if err != nil {
+			panic("err")
+		}
 
-	for _, iat := range data {
-		f.WriteString(fmt.Sprintf("%f\n", iat))
-	}
+		defer f.Close()
 
-	// SETTING UP THE TESTING SCRIPT
-	args := []string{"specification_statistical_test.py", dist, inputFile}
-	statisticalTest := exec.Command("python3.8", args...)
+		for _, iat := range data[min] {
+			f.WriteString(fmt.Sprintf("%f\n", iat))
+		}
 
-	// CALLING THE TESTING SCRIPT AND PROCESSING ITS RESULTS
-	// NOTE: the script generates a histogram in PNG format that can be used as a sanity-check
-	if err := statisticalTest.Wait(); err != nil {
-		output, _ := statisticalTest.Output()
-		fmt.Println(string(output))
+		// SETTING UP THE TESTING SCRIPT
+		args := []string{"specification_statistical_test.py", dist, inputFile, fmt.Sprintf("%f", nonScaledDuration[min])}
+		statisticalTest := exec.Command("python3.8", args...)
 
-		switch statisticalTest.ProcessState.ExitCode() {
-		case 200:
-			return true // distribution satisfied
-		case 300:
-			panic("Unsupported distribution by the statistical test.")
-		case 400:
-			return false // distribution not satisfied
+		// CALLING THE TESTING SCRIPT AND PROCESSING ITS RESULTS
+		// NOTE: the script generates a histogram in PNG format that can be used as a sanity-check
+		if err := statisticalTest.Wait(); err != nil {
+			output, _ := statisticalTest.Output()
+			fmt.Println(string(output))
+
+			switch statisticalTest.ProcessState.ExitCode() {
+			case 200:
+				result = true // distribution satisfied
+			case 300:
+				panic("Unsupported distribution by the statistical test.")
+			case 400:
+				return false // distribution not satisfied
+			}
 		}
 	}
 
-	return false
+	return result
 }
 
 func TestGenerateExecutionSpecifications(t *testing.T) {
