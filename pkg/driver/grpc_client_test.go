6255a94c1ab47ee3071ac1a12b6887c74ded063e
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2022-10-14 12:13:56 +0200

    Create cluster script new features and optimization
    Copy K8s API certificates and clone loader to all nodes
    
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/driver/grpc_client_test.go
+++ pkg/driver/grpc_client_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/eth-easl/loader/pkg/common"
 	"github.com/eth-easl/loader/pkg/workload/standard"
+	"github.com/sirupsen/logrus"
+	"os"
 	"testing"
 	"time"
 )
@@ -13,7 +15,7 @@ var testFunction = common.Function{
 }
 
 var testRuntimeSpecs = common.RuntimeSpecification{
-	Runtime: 1000, // ms
+	Runtime: 10, // ms
 	Memory:  128,
 }
 
@@ -51,5 +53,33 @@ func TestGRPCClientWithServerReachable(t *testing.T) {
 		record.ActualMemoryUsage == 0 {
 
 		t.Error("Failed gRPC invocations.")
+	}
+}
+
+func TestGRPCClientWithServerBatchWorkload(t *testing.T) {
+	logrus.SetLevel(logrus.TraceLevel)
+	os.Setenv("ITERATIONS_MULTIPLIER", "225")
+
+	address, port := "localhost", 8080
+	testFunction.Endpoint = fmt.Sprintf("%s:%d", address, port)
+
+	go standard.StartGRPCServer(address, port, standard.TraceFunction, "")
+
+	// make sure that the gRPC server is running
+	time.Sleep(2 * time.Second)
+
+	for i := 0; i < 50; i++ {
+		success, record := Invoke(&testFunction, &testRuntimeSpecs, false)
+
+		if !success ||
+			record.MemoryAllocationTimeout != false ||
+			record.ConnectionTimeout != false ||
+			record.FunctionTimeout != false ||
+			record.ResponseTime == 0 ||
+			record.ActualDuration == 0 ||
+			record.ActualMemoryUsage == 0 {
+
+			t.Error("Failed gRPC invocations.")
+		}
 	}
 }
