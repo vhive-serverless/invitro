0afd2310b997c57377dbf60c061678a90df3093f
Lightxyz <djokicm@student.ethz.ch>
2022-10-29 17:50:07 +0200

    Fixed some errors, working version without grpc
    Added unit tests for rest of functions
    Applied some review comments, tests still need improvements
    Added option to separate iat generation
    Fixed some minor issues with config and tests
    Added extra tests
    
    Signed-off-by: mihajlo djokic <djokicm@student.ethz.ch>

--- scripts/experiments/experiment_driver_test.go
+++ scripts/experiments/experiment_driver_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"github.com/sfreiberg/simplessh"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"reflect"
+	"strconv"
 	"sync"
 	"testing"
 )
@@ -16,7 +18,7 @@ var loaderCfg = loaderConfig{
 		YAMLSelector: "container",
 		EndpointPort: 80,
 
-		TracePath:          "data/traces/example/",
+		TracePath:          "data/traces",
 		OutputPathPrefix:   "data/out/experiment",
 		IATDistribution:    "exponential",
 		ExperimentDuration: 2,
@@ -33,18 +35,27 @@ var loaderCfg = loaderConfig{
 }
 
 var testDriver = Driver{
-	Username:           "",
-	ExperimentName:     "example",
-	LoaderAddresses:    []string{"localhost"},
-	clients:            nil,
-	BeginningFuncNum:   1,
-	StepSizeFunc:       1,
-	MaxFuncNum:         1,
-	ExperimentDuration: 2,
-	WorkerNodeNum:      1,
-	LocalTracePath:     "trace/example",
-	OutputDir:          "measurements/example",
-	loaderConfig:       loaderCfg,
+	Username:               "",
+	ExperimentName:         "example",
+	LoaderAddresses:        []string{"localhost"},
+	clients:                nil,
+	BeginningFuncNum:       1,
+	StepSizeFunc:           1,
+	MaxFuncNum:             1,
+	ExperimentDuration:     2,
+	WarmupDuration:         10,
+	WorkerNodeNum:          1,
+	LocalTracePath:         "trace/example",
+	LoaderTracePath:        "loader/data/traces",
+	OutputDir:              "measurements",
+	YAMLSelector:           "container",
+	IATDistribution:        "exponential",
+	LoaderOutputPath:       "data/out/experiment",
+	PartiallyPanic:         false,
+	EnableZipkinTracing:    false,
+	EnableMetricsScrapping: false,
+	SeparateIATGeneration:  false,
+	loaderConfig:           loaderCfg,
 }
 
 var testClient = connectToLoader([]string{"localhost"}, "")[0]
@@ -135,8 +146,9 @@ func TestDriver_transferFilesToLoader(t *testing.T) {
 	for _, tt := range tests {
 
 		t.Run(tt.testName, func(t *testing.T) {
+			testDriver.clients = connectToLoader(testDriver.LoaderAddresses, testDriver.Username)
 			tt.wg.Add(1)
-			testDriver.transferFilesToLoader(tt.client, tt.wg)
+			testDriver.transferFilesToLoader(tt.client, tt.wg, 0)
 			tt.wg.Wait()
 			homedir, err := os.UserHomeDir()
 			if err != nil {
@@ -146,14 +158,169 @@ func TestDriver_transferFilesToLoader(t *testing.T) {
 			if err != nil {
 				t.Errorf("Loader config file %s does not exist: %s", "loader/cmd/"+testDriver.loaderConfig.configPath, err)
 			}
-			traceFiles := [3]string{"1_inv.csv", "1_mem.csv", "1_run.csv"}
+			traceFiles := [3]string{"invocations.csv", "memory.csv", "durations.csv"}
 			for _, i := range traceFiles {
-				_, err := os.Stat(homedir + "/" + loaderTracePath + i)
+				_, err := os.Stat(homedir + "/" + testDriver.LoaderTracePath + i)
 				if err != nil {
-					t.Errorf("Trace file %s does not exist: %s", homedir+"/"+loaderTracePath+i, err)
+					t.Errorf("Trace file %s does not exist: %s", homedir+"/"+testDriver.LoaderTracePath+i, err)
 				}
 			}
 		})
 
+	}
+}
+
+func TestDriver_partitionTraceFiles(t *testing.T) {
+	tests := []struct {
+		testName string
+		driver   Driver
+		client   *simplessh.Client
+	}{
+		{
+			testName: "basic",
+			driver:   testDriver,
+			client:   testClient,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			testDriver.clients = connectToLoader(testDriver.LoaderAddresses, testDriver.Username)
+			testDriver.partitionTraceFiles()
+			traceFiles := [3]string{"1_inv_part_0.csv", "1_mem_part_0.csv", "1_run_part_0.csv"}
+			for _, i := range traceFiles {
+				path := testDriver.LocalTracePath + "/" + i
+				_, err := os.Stat(path)
+				if err != nil {
+					t.Errorf("Trace file %s does not exist: %s", path, err)
+				}
+				f, err := os.Open(path)
+				reader := csv.NewReader(f)
+				records, err := reader.ReadAll()
+				if err != nil {
+					t.Errorf("Invalid trace structure for file %s with error: %s", path, err)
+				}
+				if len(records) < 2 {
+					t.Errorf("Trace file %s is empty", path)
+				}
+			}
+		})
+	}
+}
+
+func TestDriver_prepareLoader(t *testing.T) {
+	tests := []struct {
+		testName string
+		driver   Driver
+		client   *simplessh.Client
+		wg       *sync.WaitGroup
+	}{
+		{
+			testName: "basic",
+			driver:   testDriver,
+			client:   testClient,
+			wg:       &testWg,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			testDriver.clients = connectToLoader(testDriver.LoaderAddresses, testDriver.Username)
+			tt.wg.Add(1)
+			testDriver.prepareLoader(tt.client, tt.wg)
+			tt.wg.Wait()
+			homedir, err := os.UserHomeDir()
+			if err != nil {
+				log.Fatalf("Failed to get home directory: %s", err)
+			}
+			_, err = os.Stat(homedir + "/loader/iat0.json")
+			if err != nil {
+				t.Errorf("iat file %s does not exist: %s", "/loader/iat0.json", err)
+			}
+		})
+	}
+}
+
+func TestDriver_collectStats(t *testing.T) {
+	tests := []struct {
+		testName string
+		driver   Driver
+		client   *simplessh.Client
+	}{
+		{
+			testName: "basic",
+			driver:   testDriver,
+			client:   testClient,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			testDriver.clients = connectToLoader(testDriver.LoaderAddresses, testDriver.Username)
+			testDriver.collectStats()
+			path := testDriver.OutputDir + "/" + testDriver.ExperimentName + "/experiment_duration_"
+			path = path + strconv.Itoa(testDriver.ExperimentDuration+testDriver.WarmupDuration+1) + "_"
+			path = path + strconv.Itoa(testDriver.loaderConfig.functions) + "functions_part_0.csv"
+			_, err := os.Stat(path)
+			if err != nil {
+				t.Errorf("Result file %s does not exist: %s", path, err)
+			}
+			f, err := os.Open(path)
+			reader := csv.NewReader(f)
+			records, err := reader.ReadAll()
+			if err != nil {
+				t.Errorf("Invalid structure for result file %s with error: %s", path, err)
+			}
+			if len(records) < 2 {
+				t.Errorf("Result file %s is empty", path)
+			}
+		})
+	}
+}
+
+func TestDriver_aggregateStats(t *testing.T) {
+	tests := []struct {
+		testName string
+		driver   Driver
+		client   *simplessh.Client
+	}{
+		{
+			testName: "basic",
+			driver:   testDriver,
+			client:   testClient,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			testDriver.aggregateStats()
+			path := testDriver.OutputDir + "/" + testDriver.ExperimentName + "/experiment_duration_"
+			path = path + strconv.Itoa(testDriver.ExperimentDuration+testDriver.WarmupDuration+1) + "_"
+			path = path + strconv.Itoa(testDriver.loaderConfig.functions) + "functions_aggregated.csv"
+			_, err := os.Stat(path)
+			if err != nil {
+				t.Errorf("Aggregated result file %s does not exist: %s", path, err)
+			}
+			f, err := os.Open(path)
+			reader := csv.NewReader(f)
+			records, err := reader.ReadAll()
+			if err != nil {
+				t.Errorf("Invalid structure for aggregated result file %s with error: %s", path, err)
+			}
+			pathPart := testDriver.OutputDir + "/" + testDriver.ExperimentName + "/experiment_duration_"
+			pathPart = pathPart + strconv.Itoa(testDriver.ExperimentDuration+testDriver.WarmupDuration+1) + "_"
+			pathPart = pathPart + strconv.Itoa(testDriver.loaderConfig.functions) + "functions_part_0.csv"
+			_, err = os.Stat(pathPart)
+			if err != nil {
+				t.Errorf("Partial result file %s does not exist: %s", pathPart, err)
+			}
+			filePart, err := os.Open(pathPart)
+			readerPart := csv.NewReader(filePart)
+			recordsPart, err := readerPart.ReadAll()
+			if err != nil {
+				t.Errorf("Invalid structure for result file %s with error: %s", pathPart, err)
+			}
+			if len(records) != len(recordsPart) {
+				t.Errorf("Aggregated result file %s has a different number of rows than the sum of the parts"+
+					" Part file has %d rows, aggregated result file has %d rows.",
+					path, len(recordsPart), len(records))
+			}
+		})
 	}
 }
