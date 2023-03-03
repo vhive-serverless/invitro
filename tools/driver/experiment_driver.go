dc1cd3e3d4b8d9b16f06a32a34238485917163c5
Lightxyz <djokicm@student.ethz.ch>
2023-03-03 22:42:06 +0100

    remove profiling duration

--- tools/driver/experiment_driver.go
+++ tools/driver/experiment_driver.go
@@ -366,7 +366,7 @@ func (d *Driver) collectStats() {
 	for idx, client := range clients {
 		go func(client *simplessh.Client, idx int) {
 			defer wg.Done()
-			durationInt = durationInt + d.WarmupDuration + 1 // add warmup and profiling duration
+			durationInt = durationInt + d.WarmupDuration // add warmup
 			duration := strconv.Itoa(durationInt)
 			metrics := []string{"duration"}
 			if d.loaderConfig.loaderConfiguration.EnableMetricsScrapping {
