0832834933d2c7ab30c3335e11f424ed6cb627a5
MashaNes <masa_nesic@outlook.com>
2023-05-04 15:09:15 -0600

    Addressing code cleanup instructions.
    Signed-off-by: MashaNes <masa_nesic@outlook.com>

--- pkg/driver/http_client.go
+++ pkg/driver/http_client.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os/exec"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/eth-easl/loader/pkg/common"
@@ -15,7 +16,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func InvokeOpenWhisk(function *common.Function, runtimeSpec *common.RuntimeSpecification, cfg *config.LoaderConfiguration) (bool, *mc.ExecutionRecordOpenWhisk) {
+type ActivationMetadata struct {
+	Duration  uint32 //ms
+	StartType string //"hot" or "cold"
+	WaitTime  int64  //micro seconds
+	InitTime  int64  //ms
+}
+
+func InvokeOpenWhisk(function *common.Function, runtimeSpec *common.RuntimeSpecification, cfg *config.LoaderConfiguration, AnnouceDoneExe *sync.WaitGroup) (bool, *mc.ExecutionRecordOpenWhisk) {
 	log.Tracef("(Invoke)\t %s: %d[ms], %d[MiB]", function.Name, runtimeSpec.Runtime, runtimeSpec.Memory)
 
 	record := &mc.ExecutionRecordOpenWhisk{
@@ -39,6 +47,8 @@ func InvokeOpenWhisk(function *common.Function, runtimeSpec *common.RuntimeSpeci
 		record.ResponseTime = time.Since(start).Microseconds()
 		record.ConnectionTimeout = true
 
+		AnnouceDoneExe.Done()
+
 		return false, record
 	}
 
@@ -49,6 +59,8 @@ func InvokeOpenWhisk(function *common.Function, runtimeSpec *common.RuntimeSpeci
 		record.ResponseTime = time.Since(start).Microseconds()
 		record.ConnectionTimeout = true
 
+		AnnouceDoneExe.Done()
+
 		return false, record
 	}
 
@@ -56,12 +68,17 @@ func InvokeOpenWhisk(function *common.Function, runtimeSpec *common.RuntimeSpeci
 	if record.HttpStatusCode < 200 || record.HttpStatusCode >= 300 {
 		log.Debugf("http request for function %s failed - error code: %d", function.Name, record.HttpStatusCode)
 
+		AnnouceDoneExe.Done()
+
 		return false, record
 	}
 
 	record.ActivationID = res.Header.Get("X-Openwhisk-Activation-Id")
-	record.Instance = actionName
+	record.Instance = function.Name
 	record.ResponseTime = time.Since(start).Microseconds()
+
+	AnnouceDoneExe.Done()
+	AnnouceDoneExe.Wait()
 
 	//read data from OpenWhisk based on the activation ID
 	cmd := exec.Command("wsk", "-i", "activation", "get", record.ActivationID)
@@ -75,29 +92,49 @@ func InvokeOpenWhisk(function *common.Function, runtimeSpec *common.RuntimeSpeci
 		return false, record
 	}
 
-	var jsonMap map[string]interface{}
-	output := out.String()
-	ind := strings.Index(output, "{")
-	json.Unmarshal([]byte(output[ind:]), &jsonMap)
+	err, activationMetadata := parseActivationMetadata(out.String())
+	if err != nil {
+		log.Debugf("error parsing activation metadata %s - %s", function.Name, err)
 
-	record.ActualDuration = uint32(jsonMap["duration"].(float64)) * 1000 //ms to micro sec
-	record.StartType = "hot"
-	record.InitTime = 0
-	annotations := jsonMap["annotations"].([]interface{})
-	for i := 0; i < len(annotations); i++ {
-		annotation := annotations[i].(map[string]interface{})
-
-		if annotation["key"] == "waitTime" {
-			record.WaitTime = int64(annotation["value"].(float64))
-		} else if annotation["key"] == "initTime" {
-			record.StartType = "cold"
-			record.InitTime = int64(annotation["value"].(float64)) * 1000 //ms to micro sec
-		}
+		return false, record
 	}
+
+	record.ActualDuration = activationMetadata.Duration * 1000 //ms to micro sec
+	record.StartType = activationMetadata.StartType
+	record.InitTime = activationMetadata.InitTime * 1000 //ms to micro sec
+	record.WaitTime = activationMetadata.WaitTime
 
 	log.Tracef("(Replied)\t %s: %d[ms]", function.Name, record.ActualDuration)
 	log.Tracef("(E2E Latency) %s: %.2f[ms]\n", function.Name, float64(record.ResponseTime)/1e3)
 	log.Tracef("(Client status code) %s: %d", function.Name, record.HttpStatusCode)
 
 	return true, record
+}
+
+func parseActivationMetadata(response string) (error, ActivationMetadata) {
+	var result ActivationMetadata
+	var jsonMap map[string]interface{}
+
+	ind := strings.Index(response, "{")
+	err := json.Unmarshal([]byte(response[ind:]), &jsonMap)
+	if err != nil {
+		return err, result
+	}
+
+	result.Duration = uint32(jsonMap["duration"].(float64))
+	result.StartType = "hot"
+	result.InitTime = 0
+	annotations := jsonMap["annotations"].([]interface{})
+	for i := 0; i < len(annotations); i++ {
+		annotation := annotations[i].(map[string]interface{})
+
+		if annotation["key"] == "waitTime" {
+			result.WaitTime = int64(annotation["value"].(float64))
+		} else if annotation["key"] == "initTime" {
+			result.StartType = "cold"
+			result.InitTime = int64(annotation["value"].(float64))
+		}
+	}
+
+	return nil, result
 }
