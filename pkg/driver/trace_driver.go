dbc7fae8bd2233ee4e37ac45145d309bb76a88a1
Estella Negoita <paulanegoita@yahoo.com>
2023-09-26 14:36:40 +0200

    Implement

--- pkg/driver/trace_driver.go
+++ pkg/driver/trace_driver.go
@@ -307,6 +307,8 @@ func (d *Driver) individualFunctionDriver(function *common.Function, announceFun
 			if !d.Configuration.TestMode {
 				waitForInvocations.Add(1)
 
+				rprint := &runtimeSpecification[minuteIndex][invocationIndex]
+				log.Printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>Requeste time and meme %d %d", rprint.Runtime, rprint.Memory)
 				go d.invokeFunction(&InvocationMetadata{
 					Function:              function,
 					RuntimeSpecifications: &runtimeSpecification[minuteIndex][invocationIndex],
