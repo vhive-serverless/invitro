828045e8e1656aaf8032f8cb8b6febc0f394fb94
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2023-11-09 17:38:32 +0100

    RPS generation bugfix
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/driver/trace_driver.go
+++ pkg/driver/trace_driver.go
@@ -311,13 +311,6 @@ func (d *Driver) functionsDriver(list *list.List, announceFunctionDone *sync.Wai
 			continue
 		}
 
-		offset := false
-		if IAT[iatIndex] < 0 {
-			// do not fire an invocation for offset invocations
-			IAT[iatIndex] *= -1
-			offset = true
-		}
-
 		iat := time.Duration(IAT[iatIndex]) * time.Microsecond
 
 		currentTime := time.Now()
@@ -334,7 +327,7 @@ func (d *Driver) functionsDriver(list *list.List, announceFunctionDone *sync.Wai
 			if readyToBreak {
 				break
 			}
-		} else if !offset {
+		} else {
 			if !d.Configuration.TestMode {
 				waitForInvocations.Add(1)
 
