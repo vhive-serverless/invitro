2a9d8aef752445d1afb7139cb81050a9c68d8cc3
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2023-12-04 18:01:13 +0100

    Failure for multiple nodes
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/driver/failure_trigger_test.go
+++ pkg/driver/failure_trigger_test.go
@@ -4,6 +4,6 @@ import "testing"
 
 func TestDirigentFailure(t *testing.T) {
 	//triggerDirigentFailure("cvetkovi@pc846.emulab.net", "control_plane", 0)
-	//triggerDirigentFailure("cvetkovi@pc860.emulab.net", "data_plane", 0)
+	triggerDirigentFailure("cvetkovi@hp044.utah.cloudlab.us cvetkovi@hp013.utah.cloudlab.us cvetkovi@hp052.utah.cloudlab.us cvetkovi@hp093.utah.cloudlab.us cvetkovi@hp066.utah.cloudlab.us cvetkovi@hp189.utah.cloudlab.us cvetkovi@hp065.utah.cloudlab.us cvetkovi@hp083.utah.cloudlab.us cvetkovi@hp076.utah.cloudlab.us cvetkovi@hp099.utah.cloudlab.us cvetkovi@hp060.utah.cloudlab.us cvetkovi@hp056.utah.cloudlab.us cvetkovi@hp144.utah.cloudlab.us", "data_plane", 0)
 	//triggerDirigentFailure("cvetkovi@pc727.emulab.net", "worker_node", 0)
 }
