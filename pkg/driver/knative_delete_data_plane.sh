af36d44d089d8d9372ecbb063bc89b63748d8176
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2024-03-22 12:16:00 +0100

    Data plane fail script
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/driver/knative_delete_data_plane.sh
+++ pkg/driver/knative_delete_data_plane.sh
@@ -1,8 +1,11 @@
-for s in $(kubectl get pods -n knative-serving -o name | cut -c 5- | grep activator);
-do
-  kubectl delete pod $s -n knative-serving &
-done
-for s in $(kubectl get pods -n istio-system -o name | cut -c 5- );
-do
-  kubectl delete pod $s -n istio-system &
-done
+# activator
+kubectl delete pod $(kubectl get pods -n knative-serving -o name | cut -c 5- | grep activator | tail -n 1) -n knative-serving &
+
+# cluster-local-gateway
+kubectl delete pod $(kubectl get pods -n istio-system -o name | grep cluster-local-gateway | cut -c 5- | tail -n 1) -n istio-system &
+
+# istio-ingressgateway
+kubectl delete pod $(kubectl get pods -n istio-system -o name | grep istio-ingressgateway | cut -c 5- | tail -n 1) -n istio-system &
+
+# istiod
+kubectl delete pod $(kubectl get pods -n istio-system -o name | grep istiod | cut -c 5- | tail -n 1) -n istio-system &
