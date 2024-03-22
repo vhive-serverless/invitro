ef7876d370b95c2e242a4363a06557285facc61b
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2024-03-22 12:21:30 +0100

    Delete control plane script
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/driver/knative_delete_control_plane.sh
+++ pkg/driver/knative_delete_control_plane.sh
@@ -1,8 +1,26 @@
-for s in $(kubectl get pods -n knative-serving -o name | cut -c 5- | grep -v activator);
-do
-  kubectl delete pod $s -n knative-serving &
-done
-for s in $(kubectl get pods -n kube-system -o name | cut -c 5-);
-do
-  kubectl delete pod $s -n kube-system &
-done
+# autoscaler
+kubectl delete pod $(kubectl get pods -n knative-serving -o name | cut -c 5- | grep autoscaler | tail -n 1) -n knative-serving &
+
+# controller
+kubectl delete pod $(kubectl get pods -n knative-serving -o name | cut -c 5- | grep controller | tail -n 1) -n knative-serving &
+
+# net-istio-controller
+kubectl delete pod $(kubectl get pods -n knative-serving -o name | cut -c 5- | grep net-istio-controller | tail -n 1) -n knative-serving &
+
+# net-istio-webhook
+kubectl delete pod $(kubectl get pods -n knative-serving -o name | cut -c 5- | grep net-istio-webhook | tail -n 1) -n knative-serving &
+
+# webhook
+kubectl delete pod $(kubectl get pods -n knative-serving -o name | cut -c 5- | grep webhook | tail -n 1) -n knative-serving &
+
+# etcd-node
+kubectl delete pod $(kubectl get pods -n kube-system -o name | cut -c 5- | grep etcd-node | tail -n 1) -n kube-system &
+
+# kube-apiserver
+kubectl delete pod $(kubectl get pods -n kube-system -o name | cut -c 5- | grep kube-apiserver | tail -n 1) -n kube-system &
+
+# kube-controller-manager
+kubectl delete pod $(kubectl get pods -n kube-system -o name | cut -c 5- | grep kube-controller-manager | tail -n 1) -n kube-system &
+
+# kube-scheduler
+kubectl delete pod $(kubectl get pods -n kube-system -o name | cut -c 5- | grep kube-scheduler | tail -n 1) -n kube-system &
