760014dc93ec987331fa8db22370ccf6ae44b84a
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2024-04-08 11:27:43 +0200

    Trace function
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/workload/openwhisk/workload_openwhisk.go
+++ pkg/workload/openwhisk/workload_openwhisk.go
@@ -24,13 +24,74 @@
 
 package main
 
-func Main(obj map[string]interface{}) map[string]interface{} {
-	msg := "You did not tell me who you are."
-	name, ok := obj["name"].(string)
-	if ok {
-		msg = "Hello, " + name + "!"
+import "C"
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+)
+
+const ExecUnit int = 1e2
+
+func takeSqrts() C.double {
+	var tmp C.double // Circumvent compiler optimizations
+	for i := 0; i < ExecUnit; i++ {
+		tmp = C.SQRTSD(C.double(10))
 	}
+	return tmp
+}
+
+func busySpin(multiplier, runtimeMilli uint32) {
+	totalIterations := int(multiplier * runtimeMilli)
+
+	for i := 0; i < totalIterations; i++ {
+		takeSqrts()
+	}
+}
+
+type FunctionResponse struct {
+	Status        string `json:"Status"`
+	Function      string `json:"Function"`
+	MachineName   string `json:"MachineName"`
+	ExecutionTime int64  `json:"ExecutionTime"`
+}
+
+func Main(obj map[string]interface{}) map[string]interface{} {
+	function := obj["function"].(string)
+	requestedCpu := obj["requested_cpu"].(string)
+	multiplier := obj["multiplier"].(string)
+
 	result := make(map[string]interface{})
-	result["body"] = `<html><body><h3>` + msg + `</h3></body></html>`
+
+	tlm, err := strconv.Atoi(requestedCpu)
+	if err != nil {
+		return result
+	}
+
+	mpl, err := strconv.Atoi(multiplier)
+	if err != nil {
+		return result
+	}
+
+	start := time.Now()
+	timeLeftMilliseconds := uint32(tlm)
+
+	timeConsumedMilliseconds := uint32(time.Since(start).Milliseconds())
+	if timeConsumedMilliseconds < timeLeftMilliseconds {
+		timeLeftMilliseconds -= timeConsumedMilliseconds
+		if timeLeftMilliseconds > 0 {
+			busySpin(uint32(mpl), timeLeftMilliseconds)
+		}
+	}
+
+	responseBytes, _ := json.Marshal(FunctionResponse{
+		Status:        "OK",
+		Function:      function,
+		MachineName:   "NYI",
+		ExecutionTime: time.Since(start).Microseconds(),
+	})
+
+	result["body"] = responseBytes
+
 	return result
 }
