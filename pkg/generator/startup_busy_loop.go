b5ad3145e3545ef3b16c564e13c656810fea97dc
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2024-05-16 17:04:38 +0200

    Small changes
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

---
+++ pkg/generator/startup_busy_loop.go
@@ -0,0 +1,22 @@
+package generator
+
+func StartupLoopConvertMemoryToRuntimeMs(memory int) int {
+	/*switch {
+	case memory <= 40:
+		return 0
+	case memory <= 100:
+
+	case memory <= 250:
+
+	case memory <= 500:
+
+	case memory <= 750:
+
+	case memory <= 1000:
+
+	default:
+		return 1111
+	}*/
+
+	return 0
+}
