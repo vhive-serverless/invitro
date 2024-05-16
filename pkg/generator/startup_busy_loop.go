149a5a86a33c021c6efd426f032c0e76e528caa5
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2024-05-16 17:54:18 +0200

    Distribution data for startup loop
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/generator/startup_busy_loop.go
+++ pkg/generator/startup_busy_loop.go
@@ -1,22 +1,12 @@
 package generator
 
 func StartupLoopConvertMemoryToRuntimeMs(memory int) int {
-	/*switch {
-	case memory <= 40:
-		return 0
-	case memory <= 100:
-
-	case memory <= 250:
-
-	case memory <= 500:
-
-	case memory <= 750:
-
-	case memory <= 1000:
-
-	default:
-		return 1111
-	}*/
-
-	return 0
+	// data for AWS from STeLLAR - IISWC'21
+	if memory <= 10 {
+		return 300
+	} else if memory <= 60 {
+		return 750
+	} else {
+		return 1250
+	}
 }
