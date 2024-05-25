5e23ee06f23d481b3b71323ceb773924e3581c2d
hancheng wang <wanghanchengchn@gmail.com>
2024-05-25 14:27:38 +0800

    delay kwok pod creation
    Signed-off-by: hancheng wang <wanghanchengchn@gmail.com>

--- scripts/setup/run.sh
+++ scripts/setup/run.sh
@@ -4,8 +4,9 @@ run_experiment() {
     EXP=$1
     MASTER_NODE=$2
     func=$3
+    delay=$4
 
-    export EXPERIMENT="$EXP"_$func
+    export EXPERIMENT="$EXP"_$func_$delay
     export FUNC=$func
     start_time=`date --rfc-3339=seconds | sed 's/ /T/'`
 
@@ -22,8 +23,23 @@ run_experiment() {
     make clean
 }
 
-# baseline experiment
-run_experiment "baseline" "hancheng@pc803.emulab.net" "400"
+kubectl patch stage pod-ready -p '{"spec":{"delay":{"durationMilliseconds":10000}}}' --type=merge
+run_experiment "kwok" "hancheng@pc855.emulab.net" "400" "10"
 
-# kwok experiment
-run_experiment "kwok" "hancheng@pc803.emulab.net" "400"
+kubectl patch stage pod-ready -p '{"spec":{"delay":{"durationMilliseconds":5000}}}' --type=merge
+run_experiment "kwok" "hancheng@pc855.emulab.net" "400" "5"
+
+kubectl patch stage pod-ready -p '{"spec":{"delay":{"durationMilliseconds":9000}}}' --type=merge
+run_experiment "kwok" "hancheng@pc855.emulab.net" "400" "9"
+
+kubectl patch stage pod-ready -p '{"spec":{"delay":{"durationMilliseconds":3000}}}' --type=merge
+run_experiment "kwok" "hancheng@pc855.emulab.net" "400" "3"
+
+kubectl patch stage pod-ready -p '{"spec":{"delay":{"durationMilliseconds":1000}}}' --type=merge
+run_experiment "kwok" "hancheng@pc855.emulab.net" "400" "1"
+
+kubectl patch stage pod-ready -p '{"spec":{"delay":{"durationMilliseconds":20000}}}' --type=merge
+run_experiment "kwok" "hancheng@pc855.emulab.net" "400" "20"
+
+kubectl patch stage pod-ready -p '{"spec":{"delay":{"durationMilliseconds":7000}}}' --type=merge
+run_experiment "kwok" "hancheng@pc855.emulab.net" "400" "7"
