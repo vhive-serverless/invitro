0b1053728fe5d29dcfd1bf3f9611d10af19695b8
François Costa <frankost.costa@gmail.com>
2024-05-28 18:28:26 +0200

    Fix values

--- pkg/driver/dirigent_client.go
+++ pkg/driver/dirigent_client.go
@@ -101,7 +101,7 @@ func InvokeDirigent(function *common.Function, runtimeSpec *common.RuntimeSpecif
 	log.Tracef("(Replied)\t %s: %s, %.2f[ms], %d[MiB]", function.Name, string(body), float64(0)/1e3, common.Kib2Mib(0))
 	log.Tracef("(E2E Latency) %s: %.2f[ms]\n", function.Name, float64(record.ResponseTime)/1e3)
 
-	if time.Now().Nanosecond()%1000 == 0 {
+	if time.Now().Nanosecond()%100 == 0 {
 		log.Warnf("(Replied)\t %s: %s, %.2f[ms], %d[MiB]", function.Name, string(body), float64(0)/1e3, common.Kib2Mib(0))
 		log.Warnf("(E2E Latency) %s: %.2f[ms]\n", function.Name, float64(record.ResponseTime)/1e3)
 
