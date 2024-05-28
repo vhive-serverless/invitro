abb1f37e0be4b09d2fd7d1921e88a08bfd152abd
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2024-05-28 11:57:04 +0200

    Improved error handling
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- pkg/driver/trace_driver.go
+++ pkg/driver/trace_driver.go
@@ -268,12 +268,13 @@ func (d *Driver) invokeFunction(metadata *InvocationMetadata) {
 		)
 	default:
 		log.Fatal("Unsupported platform.")
+		return
 	}
 
 	record.Phase = int(metadata.Phase)
 	record.InvocationID = composeInvocationID(d.Configuration.TraceGranularity, metadata.MinuteIndex, metadata.InvocationIndex)
 
-	if !d.Configuration.LoaderConfiguration.AsyncMode {
+	if !d.Configuration.LoaderConfiguration.AsyncMode || record.AsyncResponseGUID == "" {
 		metadata.RecordOutputChannel <- record
 	} else {
 		d.AsyncRecords.Enqueue(record)
@@ -717,7 +718,7 @@ func (d *Driver) writeAsyncRecordsToLog(logCh chan interface{}) {
 				if string(response) != "" {
 					err := deserializeDirigentResponse(response, record)
 					if err != nil {
-						log.Warnf("Failed to deserialize Dirigent response - %v - %v", string(response), err)
+						log.Errorf("Failed to deserialize Dirigent response - %v - %v", string(response), err)
 					}
 				} else {
 					record.FunctionTimeout = true
