3de6cc5cac8ad6777d00cb98be2d526962fe7361
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2024-10-15 15:59:03 +0200

    Addressing Leonid's comments
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- Makefile
+++ Makefile
@@ -39,7 +39,7 @@ build:
 	go build cmd/loader.go
 
 run:
-	go run cmd/loader.go --config cmd/config.json
+	go run cmd/loader.go --config cmd/config_knative_trace.json
 
 test:
 	go test -v -cover -race \
--- cmd/loader.go
+++ cmd/loader.go
@@ -1,17 +1,38 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 EASL and the vHive community
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/vhive-serverless/loader/pkg/common"
 	"github.com/vhive-serverless/loader/pkg/config"
 	"github.com/vhive-serverless/loader/pkg/driver"
-	"github.com/vhive-serverless/loader/pkg/generator"
 	"github.com/vhive-serverless/loader/pkg/trace"
 	"golang.org/x/exp/slices"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -24,7 +45,7 @@ const (
 )
 
 var (
-	configPath    = flag.String("config", "config.json", "Path to loader configuration file")
+	configPath    = flag.String("config", "config_knative_trace.json", "Path to loader configuration file")
 	verbosity     = flag.String("verbosity", "info", "Logging verbosity - choose from [info, debug, trace]")
 	iatGeneration = flag.Bool("iatGeneration", false, "Generate iats only or run invocations as well")
 	iatFromFile   = flag.Bool("generated", false, "True if iats were already generated")
@@ -48,6 +69,7 @@ func init() {
 		log.SetLevel(log.InfoLevel)
 	}
 }
+
 func main() {
 	cfg := config.ReadConfigurationFile(*configPath)
 	if cfg.EnableZipkinTracing {
@@ -61,15 +83,6 @@ func main() {
 	}
 	if cfg.ExperimentDuration < 1 {
 		log.Fatal("Runtime duration should be longer, at least a minute.")
-	}
-
-	if *iatGeneration {
-		durationToParse := determineDurationToParse(cfg.ExperimentDuration, cfg.WarmupDuration)
-		iatDistribution, shiftIAT := parseIATDistribution(&cfg)
-		traceParser := trace.NewAzureParser(cfg.TracePath, durationToParse)
-		functions := traceParser.Parse(cfg.Platform)
-
-		justGenerateIAT(cfg.Seed, iatDistribution, shiftIAT, parseTraceGranularity(&cfg), functions)
 	}
 
 	supportedPlatforms := []string{
@@ -90,9 +103,9 @@ func main() {
 	}
 
 	if !strings.HasSuffix(cfg.Platform, "-RPS") {
-		runTraceMode(&cfg, *iatFromFile)
+		runTraceMode(&cfg, *iatFromFile, *iatGeneration)
 	} else {
-		runRPSMode(&cfg)
+		runRPSMode(&cfg, *iatGeneration)
 	}
 }
 
@@ -156,7 +169,7 @@ func parseTraceGranularity(cfg *config.LoaderConfiguration) common.TraceGranular
 	return common.MinuteGranularity
 }
 
-func runTraceMode(cfg *config.LoaderConfiguration, readIATFromFile bool) {
+func runTraceMode(cfg *config.LoaderConfiguration, readIATFromFile bool, justGenerateIAT bool) {
 	durationToParse := determineDurationToParse(cfg.ExperimentDuration, cfg.WarmupDuration)
 	yamlPath := parseYAMLSpecification(cfg)
 
@@ -185,29 +198,9 @@ func runTraceMode(cfg *config.LoaderConfiguration, readIATFromFile bool) {
 
 	log.Infof("Using %s as a service YAML specification file.\n", experimentDriver.Configuration.YAMLPath)
 
-	experimentDriver.RunExperiment(false, readIATFromFile)
+	experimentDriver.RunExperiment(justGenerateIAT, readIATFromFile)
 }
 
-func justGenerateIAT(seed int64, iatDistribution common.IatDistribution, shiftIAT bool, traceGranularity common.TraceGranularity, functions []*common.Function) {
-	specificationGenerator := generator.NewSpecificationGenerator(seed)
-
-	for i, function := range functions {
-		spec := specificationGenerator.GenerateInvocationData(
-			function,
-			iatDistribution,
-			shiftIAT,
-			traceGranularity,
-		)
-		functions[i].Specification = spec
-
-		file, _ := json.MarshalIndent(spec, "", " ")
-		err := os.WriteFile("iat"+strconv.Itoa(i)+".json", file, 0644)
-		if err != nil {
-			log.Fatalf("Writing the loader config file failed: %s", err)
-		}
-	}
-}
-
-func runRPSMode(cfg *config.LoaderConfiguration) {
+func runRPSMode(cfg *config.LoaderConfiguration, justGenerateIAT bool) {
 	panic("Not yet implemented")
 }
