81e8e3c5eeba8d996c4f16a9fd3a41a90898f3d1
KarthikL1729 <karthiklaksh1729@gmail.com>
2024-10-23 15:33:20 +0530

    Added flag for vSwarm functions in the deployment pipeline. Invoker changes to invoke invitro functions pending.
    Signed-off-by: L Lakshmanan <lakshmanan.l@research.iiit.ac.in>

--- automated_deployer.sh
+++ automated_deployer.sh
@@ -4,13 +4,14 @@
 # and the OutputPathPrefix field as: "data/out/$EXPERIMENT/experiment"
 
 # Run the script as follows:
-# ./automated_deployer.sh <start number of functions> <stop number of functions> <step size> <experiment name>
+# ./automated_deployer.sh <start number of functions> <stop number of functions> <step size> <experiment name> <vSwarm flag>
 
 
 start=$1
 stop=$2
 step=$3
 EXPERIMENT=$4
+vSwarm=$5
 
 counter=$start
 
@@ -24,13 +25,24 @@ do
         counter=$((counter + step))
         continue
     fi
-    cd pkg/mapper/
-    python3 mapper.py -t ../../data/traces/reference/sampled_150/$counter/ -p profile.json
-    cd ../..
-    cat cmd/config.json | envsubst '${EXPERIMENT},${FUNC}' > cmd/config_tmp.json
-    mkdir -p data/out/$EXPERIMENT
-    go run cmd/loader.go -config cmd/config_tmp.json -verbosity debug | tee data/out/$EXPERIMENT/loader_$FUNC.log
+    if [ $vSwarm -eq 1 ]; then
+        echo "Using vSwarm functions"
+        cd pkg/mapper/
+        python3 mapper.py -t ../../data/traces/reference/sampled_150/$counter/ -p profile.json
+        cd ../..
+        cat cmd/config.json | envsubst '${EXPERIMENT},${FUNC}' > cmd/config_tmp.json
+        mkdir -p data/out/$EXPERIMENT
+        go run cmd/loader.go -config cmd/config_tmp.json -verbosity debug -vSwarm true | tee data/out/$EXPERIMENT/loader_$FUNC.log
+
+    else
+        echo "Using dummy functions"
+        cat cmd/config.json | envsubst '${EXPERIMENT},${FUNC}' > cmd/config_tmp.json
+        mkdir -p data/out/$EXPERIMENT
+        go run cmd/loader.go -config cmd/config_tmp.json -verbosity debug | tee data/out/$EXPERIMENT/loader_$FUNC.log
+    fi
     counter=$((counter + step))
 done
+
+rm cmd/config_tmp.json
 
 echo "Done with the automation script"
