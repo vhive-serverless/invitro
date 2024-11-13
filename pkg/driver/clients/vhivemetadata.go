df997aab091a96272ba0cb1240112f6bb85c1d8e
aryans1204 <arshar1204@gmail.com>
2024-11-13 10:32:51 -0600

    Tweaked invoker and added tests
    Signed-off-by: aryans1204 <arshar1204@gmail.com>
    
    Add VHIVE_REPO and LOADER_REPO to setup script config
    
    Signed-off-by: Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
    
    Added tests and created new vSwarm invoker
    
    Fixed linting
    
    Fixed VSwarm Invoker for Knative mode
    
    Committer: aryans1204 arshar1204@gmail.com
    
    Fixed VSwarm Invoker for Knative mode
    
    Added new invoker for vSwarm functions and added tests
    
    Signed-off-by: aryans1204 <arshar1204@gmail.com>
    
    Fixed formatting under grpc_client.go
    
    Signed-off-by: aryans1204 <arshar1204@gmail.com>
    
    Fixed driver tests
    
    Signed-off-by: aryans1204 <arshar1204@gmail.com>

---
+++ pkg/driver/clients/vhivemetadata.go
@@ -0,0 +1,58 @@
+package clients
+
+import (
+	"encoding/json"
+	"time"
+
+	ctrdlog "github.com/containerd/log"
+	"github.com/sirupsen/logrus"
+)
+
+type vHiveMetadata struct {
+	WorkflowId   string    `json:"WorkflowId"`
+	InvocationId string    `json:"InvocationId"`
+	InvokedOn    time.Time `json:"InvokedOn"`
+}
+
+func GetWorkflowId(d []byte) string {
+	return unmarshalVHiveMetadata(d).WorkflowId
+}
+
+func GetInvocationId(d []byte) string {
+	return unmarshalVHiveMetadata(d).InvocationId
+}
+
+func GetInvokedOn(d []byte) time.Time {
+	return unmarshalVHiveMetadata(d).InvokedOn
+}
+
+func unmarshalVHiveMetadata(d []byte) (vhm vHiveMetadata) {
+	if err := json.Unmarshal(d, &vhm); err != nil {
+		logrus.Fatal("failed to unmarshal vhivemetadata", err)
+	}
+	return
+}
+
+func MakeVHiveMetadata(WorkflowId, InvocationId string, InvokedOn time.Time) []byte {
+	return marshalVHiveMetadata(vHiveMetadata{
+		WorkflowId:   WorkflowId,
+		InvocationId: InvocationId,
+		InvokedOn:    InvokedOn,
+	})
+}
+
+func marshalVHiveMetadata(vhm vHiveMetadata) []byte {
+	d, err := json.Marshal(struct {
+		WorkflowId   string `json:"WorkflowId"`
+		InvocationId string `json:"InvocationId"`
+		InvokedOn    string `json:"InvokedOn"`
+	}{
+		WorkflowId:   vhm.WorkflowId,
+		InvocationId: vhm.InvocationId,
+		InvokedOn:    vhm.InvokedOn.Format(ctrdlog.RFC3339NanoFixed),
+	})
+	if err != nil {
+		logrus.Fatal("failed to marshal vHiveMetadata", err)
+	}
+	return d
+}
