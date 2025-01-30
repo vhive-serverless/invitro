b62402d2d8a64d7e239e835cfbe690053c8767f6
L Lakshmanan <s2760012@ed.ac.uk>
2025-01-30 17:41:41 +0000

    Addressing comments for the mapper tool.
    Signed-off-by: L Lakshmanan <s2760012@ed.ac.uk>
    
    Addressed review comments.
    
    Signed-off-by: L Lakshmanan <s2760012@ed.ac.uk>

--- tools/mapper/mapper.py
+++ tools/mapper/mapper.py
@@ -4,12 +4,12 @@ import json
 import re
 import argparse
 import pandas as pd
-import matplotlib.pyplot as plt
 from find_proxy_function import *
 
 from log_config import *
 
 INVOCATION_COLUMN = 4
+VSWARM_MAX_DUR = 27000
 
 def load_trace(trace_directorypath):
     duration_info = {}
@@ -27,6 +27,9 @@ def load_trace(trace_directorypath):
                 f"Durations file {duration_filepath} cannot be read. Error: {e}"
             )
             return None, -1
+    else:
+        log.critical(f"Durations file {duration_filepath} not found")
+        return None, -1
 
     # Read the memory file
     memory_filepath = trace_directorypath + "/memory.csv"
@@ -39,6 +42,9 @@ def load_trace(trace_directorypath):
                 f"Memory file {memory_filepath} cannot be read. Error: {e}"
             )
             return None, -1
+    else:
+        log.critical(f"Memory file {memory_filepath} not found")
+        return None, -1
         
     # Rename all columns in the dataframe with a lambda (for example: percentile_Average_1 -> 1-percentile) if x matches a regex
     duration_info = duration_info.rename(columns=lambda x: re.sub(r'percentile_(\w+)_(\d+)', r'\2-' + "percentile", x))
@@ -49,94 +55,15 @@ def load_trace(trace_directorypath):
     trace_functions = {}
 
     for _, row in duration_info.iterrows():
-        hash_function = row["HashFunction"]
-        trace_functions[hash_function] = {}
-        trace_functions[hash_function]["duration"] = row
+        unique_id = row["HashFunction"] + row["HashOwner"] + row["HashApp"]
+        trace_functions[unique_id] = {}
+        trace_functions[unique_id]["duration"] = row
 
     for _, row in memory_info.iterrows():
-        hash_function = row["HashFunction"]
-        trace_functions[hash_function]["memory"] = row
+        unique_id = row["HashFunction"] + row["HashOwner"] + row["HashApp"]
+        trace_functions[unique_id]["memory"] = row
 
     return trace_functions, 0
-
-def generate_plot(trace_directorypath, profile_filepath, output_filepath, invoke=True):
-    trace_functions, err = load_trace(trace_directorypath)
-    if err == -1:
-        log.critical(f"Load Generation failed")
-        return
-    elif err == 0:
-        log.info(f"Trace loaded")
-
-    ## Check whether the profile file for proxy functions exists or not
-    if os.path.exists(profile_filepath):
-        log.info(
-            f"Profile file for proxy functions {profile_filepath} exists. Accessing information"
-        )
-        try:
-            with open(profile_filepath, "r") as jf:
-                proxy_functions = json.load(jf)
-        except Exception as e:
-            log.critical(
-                f"Profile file for proxy functions {profile_filepath} cannot be read. Error: {e}"
-            )
-            log.critical(f"Load Generation failed")
-            return
-    else:
-        log.critical(f"Profile file for proxy functions {profile_filepath} not found")
-        log.critical(f"Load Generation failed")
-        return
-    
-    if os.path.exists(output_filepath):
-        log.info(
-            f"Mapper output file for trace functions {output_filepath} exists. Accessing information"
-        )
-        try:
-            with open(output_filepath, "r") as jf:
-                mapped_traces = json.load(jf)
-        except Exception as e:
-            log.critical(
-                f"Mapper output file for trace functions {output_filepath} cannot be read. Error: {e}"
-            )
-            log.critical(f"Load Generation failed")
-            return
-    else:
-        log.critical(f"Mapper output file for trace functions {output_filepath} not found")
-        log.critical(f"Load Generation failed")
-        return
-    
-    if invoke:
-        invocations = pd.read_csv(trace_directorypath+"/invocations.csv")
-        inv_df = {}
-        for i in range(len(invocations)):
-            inv_df[invocations["HashFunction"][i]] = sum([invocations[col][i] if col not in invocations.columns[0:INVOCATION_COLUMN] else 0 for col in invocations.columns])
-        
-        trace_durations = []
-        mapped_durations = []
-        dropped_functions, dropped_invocations, total_invocations = 0, 0, 0
-        for trace in trace_functions:
-            duration = trace_functions[trace]["duration"]["50-percentile"]
-            if duration > 27000:
-                dropped_functions += 1
-                dropped_invocations += inv_df[trace]
-                continue
-            total_invocations += inv_df[trace]
-            proxy_name = mapped_traces[trace]["proxy-function"]
-            profile_duration = proxy_functions[proxy_name]["duration"]["50-percentile"]
-            trace_durations.append(duration)
-            mapped_durations.append(profile_duration)
-        return trace_durations, mapped_durations, dropped_functions, (dropped_invocations/total_invocations)*100
-    else:
-        trace_durations = []
-        mapped_durations = []
-        for trace in trace_functions:
-            duration = trace_functions[trace]["duration"]["50-percentile"]
-            if duration > 27000:
-                continue
-            proxy_name = mapped_traces[trace]["proxy-function"]
-            profile_duration = proxy_functions[proxy_name]["duration"]["50-percentile"]
-            trace_durations.append(duration)
-            mapped_durations.append(profile_duration)
-        return trace_durations, mapped_durations
 
 def main():
     # Parse the arguments
@@ -161,7 +88,7 @@ def main():
     output_filepath = trace_directorypath + "/mapper_output.json"
     trace_functions, err = load_trace(trace_directorypath)
     if err == -1:
-        log.critical(f"Load Generation failed")
+        log.critical(f"Trace loading failed")
         return
     elif err == 0:
         log.info(f"Trace loaded")
@@ -186,23 +113,20 @@ def main():
         return
 
     # Getting a proxy function for every trace function
-    trace_functions, err = get_proxy_function(
+    trace_functions = get_closest_proxy_function(
         trace_functions=trace_functions,
         proxy_functions=proxy_functions,
     )
-    if err == -1:
-        log.critical(f"Load Generation failed")
-        return
-    elif err == 0:
-        log.info(f"Proxy functions obtained")
+    
+    log.info(f"Proxy functions obtained")
 
     # Writing the proxy functions to a file
 
     # Only give function name and proxy name
     trace_json = {}
-    for function in trace_functions:
-        trace_json[function] = {}
-        trace_json[function]["proxy-function"] = trace_functions[function]["proxy-function"] 
+    for id in trace_functions:
+        trace_json[id] = {}
+        trace_json[id]["proxy-function"] = trace_functions[id]["proxy-function"] 
 
     try:
         with open(output_filepath, "w") as jf:
@@ -213,15 +137,7 @@ def main():
         return
     
     log.info(f"Output file {output_filepath} written")
-    log.info(f"Preliminary load Generation successful. Checking error levels in mapping...")
-
-    # Read the mapper output
-    try:
-        with open(output_filepath, "r") as jf:
-            mapper_output = json.load(jf)
-    except Exception as e:
-        log.critical(f"Error in loading mapper output file {e}")
-        return
+    log.info(f"Load Generation successful. Mapper output generated.")
 
     # Check the memory and duration errors
 
@@ -235,17 +151,18 @@ def main():
     abs_rel_mem_error = 0
     abs_rel_dur_error = 0
     zero_duration = 0
-    for function in mapper_output:
-        trace_mem = trace_functions[function]["memory"]["50-percentile"]
-        trace_dur = trace_functions[function]["duration"]["50-percentile"]
-        proxy_dur = proxy_functions[mapper_output[function]["proxy-function"]]["duration"]["50-percentile"]
-        proxy_mem = proxy_functions[mapper_output[function]["proxy-function"]]["memory"]["50-percentile"]
-        log.warning(f"Memory error for function {function} is {abs(trace_mem - proxy_mem)} MB per invocation")
+    for id in trace_functions:
+        if trace_functions[id]["proxy-function"] == "trace-func-go":
+            dur_count += 1
+            continue
+        trace_mem = trace_functions[id]["memory"]["50-percentile"]
+        trace_dur = trace_functions[id]["duration"]["50-percentile"]
+        proxy_dur = proxy_functions[trace_functions[id]["proxy-function"]]["duration"]["50-percentile"]
+        proxy_mem = proxy_functions[trace_functions[id]["proxy-function"]]["memory"]["50-percentile"]
         mem_error += trace_mem - proxy_mem
         rel_mem_error += (trace_mem - proxy_mem)/trace_mem
         abs_mem_error += abs(trace_mem - proxy_mem)
         abs_rel_mem_error += abs((trace_mem - proxy_mem)/trace_mem)
-        log.warning(f"Duration error for function {function} is {abs(trace_dur - proxy_dur)}ms per invocation")
         dur_error += trace_dur - proxy_dur
         abs_dur_error += abs(trace_dur - proxy_dur)
         if trace_dur == 0:
@@ -253,11 +170,8 @@ def main():
         else:
             rel_dur_error += (trace_dur - proxy_dur)/trace_dur
             abs_rel_dur_error += abs((trace_dur - proxy_dur)/trace_dur)
-        if abs(trace_dur - proxy_dur) > 0.4*trace_dur:
-            mapper_output[function]["proxy-function"] = "trace-func-go"
-            dur_count += 1
 
-    total_functions = len(mapper_output)
+    total_functions = len(trace_functions)
     log.info(f"Average memory error: {mem_error/total_functions} MB per invocation")
     log.info(f"Average duration error: {dur_error/total_functions} ms per invocation")
     log.info(f"Average absolute memory error: {abs_mem_error/total_functions} MB per invocation")
@@ -268,19 +182,6 @@ def main():
     log.info(f"Average absolute relative duration error: {abs_rel_dur_error/total_functions}")
     log.info(f"Duration errors: {dur_count}")
     log.info(f"Functions with 0 duration: {zero_duration}")
-
-    log.info(f"Replacing the functions with high duration error with invitro functions.")
-
-    ## Write the updated mapper output
-
-    try:
-        with open(output_filepath, "w") as jf:
-            json.dump(mapper_output, jf, indent=4)
-            log.info(f"Updated output file {output_filepath} written. Load generated.")
-    except Exception as e:
-        log.critical(f"Output file {output_filepath} cannot be written. Error: {e}")
-        log.critical(f"Load Generation failed")
-        return
 
 if __name__ == "__main__":
     main()
