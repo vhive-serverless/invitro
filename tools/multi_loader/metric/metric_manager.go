59265c3f1e3f1c5b76cdb9b5f3d1a57410cc4338
Lenson <nosnelmil@gmail.com>
2025-03-03 15:42:37 +0800

    add log consolidation logic
    Signed-off-by: Lenson <nosnelmil@gmail.com>

--- tools/multi_loader/metric/metric_manager.go
+++ tools/multi_loader/metric/metric_manager.go
@@ -1,11 +1,13 @@
 package metric
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"sync"
@@ -22,6 +24,7 @@ const (
 	AUTOSCALER_DIR_NAME = "autoscaler"
 	ACTIVATOR_DIR_NAME  = "activator"
 	PROMETH_DIR_NAME    = "prometheus_snapshot"
+	TIMESTAMP_FORMAT    = "20060102-150405"
 )
 
 type MetricManager struct {
@@ -29,6 +32,7 @@ type MetricManager struct {
 	metricsToCollect  []string
 	outputDir         string
 	multiLoaderConfig types.MultiLoaderConfiguration
+	startTime         time.Time
 }
 
 func NewMetricManager(platform string, multiLoaderConfig types.MultiLoaderConfiguration) *MetricManager {
@@ -55,11 +59,11 @@ func (m *MetricManager) CollectMetrics() {
 	}
 
 	if m.shouldCollect(ml_common.AutoScaler) {
-		m.collectAutoScalerLogs()
+		m.collectPodLogs(m.multiLoaderConfig.AutoScalerNode, ml_common.AutoScalerPod.String(), ACTIVATOR_DIR_NAME)
 	}
 
 	if m.shouldCollect(ml_common.Activator) {
-		m.collectActivatorLogs()
+		m.collectPodLogs(m.multiLoaderConfig.ActivatorNode, ml_common.ActivatorPod.String(), AUTOSCALER_DIR_NAME)
 	}
 
 	if m.shouldCollect(ml_common.Prometheus) {
@@ -68,9 +72,19 @@ func (m *MetricManager) CollectMetrics() {
 }
 
 /**
-* Resets top processes in all nodes
+* Reset the start time from which log/metrics should be collected and TOP for all nodes
 **/
-func (m *MetricManager) ResetTOP() {
+func (m *MetricManager) Reset() {
+	// Reset TOP for all nodes
+	m.resetTOP()
+	// Set the start time for logs collected
+	m.startTime = time.Now()
+}
+
+/**
+* Resets top processes in all nodesa
+**/
+func (m *MetricManager) resetTOP() {
 	log.Debug("Resetting top process")
 	// Check if should reset
 	if !m.shouldCollect(ml_common.TOP) {
@@ -153,36 +167,126 @@ func (m *MetricManager) collectTOPMetric() {
 }
 
 /**
-* Collects autoscaler logs from autoscaler node
+* Collects logs from a specific pod
 **/
-func (m *MetricManager) collectAutoScalerLogs() {
-	log.Debug("Collecting autoscaler logs from node: ", m.multiLoaderConfig.AutoScalerNode)
-	autoScalerOutputDir := path.Join(m.outputDir, AUTOSCALER_DIR_NAME)
-	err := os.MkdirAll(autoScalerOutputDir, 0755)
+func (m *MetricManager) collectPodLogs(podIP string, podName string, outputDirName string) {
+	log.Debugf("Collecting %s logs from %s", podName, podIP)
+	outputDir := path.Join(m.outputDir, outputDirName)
+	err := os.MkdirAll(outputDir, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Retrieve autoscaler logs
-	err = ml_common.CopyRemoteFile(m.multiLoaderConfig.AutoScalerNode, "/var/log/pods/knative-serving_autoscaler-*/autoscaler/*", autoScalerOutputDir)
-	if err != nil {
-		log.Fatal("Failed to copy autoscaler logs from node: ", m.multiLoaderConfig.AutoScalerNode, err)
-	}
-}
-
-/**
-* Collects activator logs from activator node
-**/
-func (m *MetricManager) collectActivatorLogs() {
-	log.Debug("Collecting activator logs from node: ", m.multiLoaderConfig.ActivatorNode)
-	activatorOutputDir := path.Join(m.outputDir, ACTIVATOR_DIR_NAME)
-	err := os.MkdirAll(activatorOutputDir, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Retrieve activator logs
-	err = ml_common.CopyRemoteFile(m.multiLoaderConfig.ActivatorNode, "/var/log/pods/knative-serving_activator-*/activator/*", activatorOutputDir)
+	// Retrieve logs
+	err = ml_common.CopyRemoteFile(m.multiLoaderConfig.ActivatorNode, fmt.Sprintf("/var/log/pods/knative-serving_%s-*/*/*", podName), outputDir)
 	if err != nil {
 		log.Fatal("Failed to copy activator logs from node: ", m.multiLoaderConfig.ActivatorNode, err)
+	}
+	// Check if output dir contains anything
+	files, err := os.ReadDir(outputDir)
+	if err != nil {
+		log.Fatal("Unexpected error. Failed to read directory ", outputDir)
+	}
+	if len(files) == 0 {
+		log.Warnf("No logs were found for pod %s in directory %s", podName, outputDir)
+	} else {
+		log.Debugf("Successfully retrieved %d logs for pod %s in directory %s", len(files), podName, outputDir)
+	}
+	m.consolidateLogs(podName, outputDir)
+}
+
+func (m *MetricManager) consolidateLogs(podName string, logDir string) {
+	files, err := os.ReadDir(logDir)
+	if err != nil {
+		log.Fatal("Unexpected error. Failed to read directory ", logDir)
+	}
+	// Remove outdated rotated log files
+	thresholdStartTime := m.startTime.Format(TIMESTAMP_FORMAT)
+	timestampRegex := regexp.MustCompile(`(\d{8}-\d{6})`)
+	log.Debug("Threshold Start timestamp:", thresholdStartTime)
+	if err != nil {
+		log.Fatal("Error parsing startime: ", m.startTime, err)
+	}
+
+	// Filtering out based on file name
+	for _, file := range files {
+		fileName := file.Name()
+		logFilePath := path.Join(logDir, fileName)
+		log.Debug("Checking timestamp in filename: ", fileName)
+
+		if file.IsDir() || !strings.HasSuffix(fileName, ".gz") {
+			continue
+		}
+
+		// Check rotated logs timestamp
+		matches := timestampRegex.FindStringSubmatch(fileName)
+		if len(matches) > 0 {
+			if matches[1] < thresholdStartTime {
+				os.Remove(logFilePath)
+				continue
+			}
+		}
+
+		// unzip compressed log file
+		_, err := ml_common.DecompressGZFile(logFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	// Consolidate current log and rotated log files and remove outdated lines of logs
+	logOutputPath := path.Join(logDir, fmt.Sprintf("knative-serving_%s.log", podName))
+	outputFile, err := os.Create(logOutputPath)
+	if err != nil {
+		log.Fatal("Failed to create output file: ", outputFile, err)
+	}
+	defer outputFile.Close()
+
+	// Filter out logs based on each log line timestamp
+	err = filepath.Walk(logDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() || path == logOutputPath {
+			return err
+		}
+		log.Debug("Reading and checking timestamps in log file: ", path)
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			// Compare timestamps
+			fields := strings.Fields(line)
+			if fields[0] > m.startTime.Format(time.RFC3339Nano) {
+				if len(fields) > 3 {
+					fmt.Fprintln(outputFile, strings.Join(fields[3:], " "))
+				}
+			}
+		}
+		return scanner.Err()
+	})
+	if err != nil {
+		log.Printf("Error processing log files: %v", err)
+	}
+	// Remove all other log files
+	files, err = os.ReadDir(logDir)
+	if err != nil {
+		log.Fatal("Failed to read directory: ", logDir, err)
+	}
+	for _, file := range files {
+		if file.Name() != fmt.Sprintf("knative-serving_%s.log", podName) {
+			err := os.RemoveAll(path.Join(logDir, file.Name()))
+			if err != nil {
+				log.Fatal("Error occured when removing log files", err)
+			}
+		}
+	}
+	// Check output
+	if stat, _ := outputFile.Stat(); stat.Size() == 0 {
+		log.Warnf("No logs found for pod %s after filtering", podName)
+	} else {
+		log.Debugf("Collected logs for pod %s in %s", podName, logOutputPath)
 	}
 }
 
