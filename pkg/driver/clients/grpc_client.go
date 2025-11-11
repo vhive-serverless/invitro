4c5cd31a33dbed7a7de22e3f0699afb13e22bc34
JooYoung Park <jooyoung.park73@gmail.com>
2025-11-10 23:16:24 -0700

    add all payload to prefetching argument
    Signed-off-by: JooYoung Park <jooyoung.park73@gmail.com>

--- pkg/driver/clients/grpc_client.go
+++ pkg/driver/clients/grpc_client.go
@@ -72,7 +72,8 @@ var FunctionPayloads = map[string][]string{
 	"rnnserve": {"input_payload/rnn_serving/rnn_serving_input.txt"},
 	"streducer": {"input_payload/stack_training-reducer/KNeighborsRegressor.pkl", "input_payload/stack_training-reducer/Lasso.pkl",
 		"input_payload/stack_training-reducer/LinearRegression.pkl", "input_payload/stack_training-reducer/LinearSVR.pkl",
-		"input_payload/stack_training-reducer/RandomForestRegressor.pkl"},
+		"input_payload/stack_training-reducer/RandomForestRegressor.pkl", "input_payload/stack_training-reducer/KNeighborsRegressor_y_pred.pkl", "input_payload/stack_training-reducer/Lasso_y_pred.pkl", 
+	"input_payload/stack_training-reducer/LinearRegression_y_pred.pkl", "input_payload/stack_training-reducer/LinearSVR_y_pred.pkl", "input_payload/stack_training-reducer/RandomForestRegressor_y_pred.pkl"},
 	"sttrainer": {"input_payload/stack_training-trainer/dataset"},
 }
 
